f677f7111e5b733f1c4beb12f0490617adfe15ff
Ellen Zhong <zhonge@mit.edu>
2018-11-19 12:55:06 -0500

    Add missing imports

--- lib-python/utils.py
+++ lib-python/utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime as dt
+import os, sys
+import numpy as np
 
 _verbose = False
 
@@ -10,4 +12,16 @@ def vlog(msg):
     if _verbose:
         print('{}     {}'.format(dt.now().strftime('%Y-%m-%d %H:%M:%S'), msg))
         sys.stdout.flush()
+
+def R_from_eman(a,b,y):
+    a *= np.pi/180.
+    b *= np.pi/180.
+    y *= np.pi/180.
+    ca, sa = np.cos(a), np.sin(a)
+    cb, sb = np.cos(b), np.sin(b)
+    cy, sy = np.cos(y), np.sin(y)
+    Ra = np.array([[ca,-sa,0],[sa,ca,0],[0,0,1]])
+    Rb = np.array([[1,0,0],[0,cb,-sb],[0,sb,cb]])
+    Ry = np.array(([cy,-sy,0],[sy,cy,0],[0,0,1]))
+    return np.dot(np.dot(Ry,Rb),Ra)
 

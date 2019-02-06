3b05a5c587a020a361992998f91f9b907bce5979
Ellen Zhong <zhonge@mit.edu>
2019-02-06 09:44:26 -0500

    Update testing script

--- testing/test.sh
+++ testing/test.sh
@@ -2,4 +2,6 @@ python ../backproject.py data/toy_projections.mrcs data/toy_angles.pkl -o output
 chimera output/toy_recon.mrc data/toymodel_small_nocenter.mrc 
 python ../backproject_nn.py data/toy_projections.mrcs data/toy_angles.pkl -o output/toy_nn_recon -n 10
 python ../backproject_hartley.py data/toy_projections.mrcs data/toy_angles.pkl -o output/toy_recon_hartley.mrc
-python ../vae_rot.py  data/toy_projections.mrcs data/toy_angles.pkl -o output/toy_recon_vae --lr .00001 --save-intermediate
+python ../vae_rot.py  data/toy_projections.mrcs -o output/toy_recon_vae --lr .0001 
+python ../vae_rot.py  data/aic_projections_small.mrc -o output/aic_projections --lr .0001 -n 10
+

42a0016b3491091abdd7deeaeb40bfbdff1ad49c
Ellen Zhong <zhonge@mit.edu>
2019-02-24 17:23:12 -0500

    Bug fix in getting the nearest neighbors on the grid
    The Hopf fiber bundle grid is weird. The nearest neighbors
    on S2/S1 do not necessarily lead to the nearest
    neighbors on SO3

--- lib-python/so3_grid.py
+++ lib-python/so3_grid.py
@@ -30,14 +30,13 @@ def hopf_to_quat(theta, phi, psi):
 	'''
 	ct = np.cos(theta/2)
 	st = np.sin(theta/2)
-	quat = np.empty((len(theta),4))
-	quat[:,0] = ct*np.cos(psi/2)
-	quat[:,1] = ct*np.sin(psi/2)
-	quat[:,2] = st*np.cos(phi+psi/2)
-	quat[:,3] = st*np.sin(phi+psi/2)
-	return quat
+	quat = np.array([ct*np.cos(psi/2),
+					 ct*np.sin(psi/2),
+					 st*np.cos(phi+psi/2),
+					 st*np.sin(phi+psi/2)])
+	return quat.T
 
-def SO3_grid(resol):
+def grid_SO3(resol):
 	theta, phi = grid_s2(resol)
 	psi = grid_s1(resol)
 	quat = hopf_to_quat(np.repeat(theta,len(psi)),
@@ -46,36 +45,47 @@ def SO3_grid(resol):
 	return quat #hmm convert to rot matrix?
 
 def base_SO3_grid():
-	return SO3_grid(0)
+	return grid_SO3(0)
 
 #### Neighbor finding ####
 
 def get_s1_neighbor(mini, curr_res):
 	Npix = 6*2**(curr_res+1)
 	dt = 2*np.pi/Npix
-	return np.array([2*mini, 2*mini+1])*dt + dt/2
+	#return np.array([2*mini, 2*mini+1])*dt + dt/2
+	# the fiber bundle grid on SO3 is weird
+	# the next resolution level's nearest neighbors in SO3 are not 
+	# necessarily the nearest neighbor grid points in S1
+	# include the 13 neighbors for now... eventually learn/memoize the mapping 
+	ind = np.arange(2*mini-1, 2*mini+3)
+	if ind[0] < 0:
+		ind[0] += Npix
+	return ind*dt+dt/2, ind
 
 def get_s2_neighbor(mini, curr_res):
 	Nside = 2**(curr_res+1)
-	return hp.pix2ang(Nside, np.arange(4)+4*mini, nest=True)
+	ind = np.arange(4)+4*mini
+	return hp.pix2ang(Nside, ind, nest=True), ind
 
-def find_ind_base(mini):
+def get_base_ind(mini):
 	psii = mini%6
 	thetai = int(mini/6)
 	return thetai, psii
 
-def get_neighbor(s2i, s1i, curr_res):
-	theta, phi = get_s2_neighbor(s2i, curr_res)
-	s2_nexti = np.arange(4)+4*s2i
-	psi = get_s1_neighbor(s1i, curr_res)
-	s1_nexti = np.arange(2)+2*s1i
-	quat = hopf_to_quat(np.repeat(theta,len(psi)),
+def get_neighbor(quat, s2i, s1i, curr_res):
+	(theta, phi), s2_nexti = get_s2_neighbor(s2i, curr_res)
+	psi, s1_nexti = get_s1_neighbor(s1i, curr_res)
+	quat_n = hopf_to_quat(np.repeat(theta,len(psi)),
 						np.repeat(phi,len(psi)),
 						np.tile(psi,len(theta)))
 	ind = np.array([np.repeat(s2_nexti,len(psi)),
 					np.tile(s1_nexti, len(theta))])
 	ind = ind.T
-	return quat, ind
+	# find the 8 nearest neighbors of 16 possible points
+	# need to check distance from both +q and -q
+	dists = np.minimum(np.sum((quat_n-quat)**2,axis=1), np.sum((quat_n+quat)**2,axis=1))
+	ii = np.argsort(dists)[:8]
+	return quat_n[ii], ind[ii]
 
 
 

6f865886ca0a9097c19c5e193ffd91d0e1285d1d
Ellen Zhong <zhonge@mit.edu>
2019-03-20 12:03:56 -0400

    Bug fix -- not using tilted image

--- lib-python/models.py
+++ lib-python/models.py
@@ -81,7 +81,7 @@ class TiltVAE(nn.Module):
         # tilt series pair
         x = x @ self.tilt
         y_hat2 = self.decoder(x)
-        y_hat2 = y_hat.view(-1, self.ny, self.nx)
+        y_hat2 = y_hat2.view(-1, self.ny, self.nx)
         return y_hat, y_hat2, z_mu, z_std, w_eps
 
 

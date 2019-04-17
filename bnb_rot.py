ad85684c5cbb66665e7e3e06490e738dd709a37b
Ellen Zhong <ellen.zhong@virginia.edu>
2019-04-17 11:38:55 -0400

    Refactor bnb_rot.py to use new Dataset classes
    Also refactor training loop to be in its own function

--- bnb_rot.py
+++ bnb_rot.py
@@ -10,14 +10,14 @@ from datetime import datetime as dt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-from torch.distributions import Normal
+from torch.utils.data import DataLoader
 
 sys.path.insert(0,os.path.abspath(os.path.dirname(__file__))+'/lib-python')
 import mrc
 import utils
 import fft
-import lie_tools
+import dataset
+
 from lattice import Lattice
 from bnb import BNNBHomo
 from models import FTSliceDecoder
@@ -56,8 +56,8 @@ def parse_args():
 
     return parser
 
-def eval_volume(model, lattice, D, rnorm):
-    '''Evaluate the model on a nz x ny x nx lattice'''
+def eval_volume(model, lattice, D, norm):
+    '''Evaluate the model on a DxDxD lattice'''
     vol_f = np.zeros((D,D,D),dtype=np.float32)
     assert not model.training
     # evaluate the volume by zslice to avoid memory overflows
@@ -66,16 +66,46 @@ def eval_volume(model, lattice, D, rnorm):
         with torch.no_grad():
             y = model.decode(x)
             y = y[...,0] - y[...,1]
-            #y = model(x)
             y = y.view(D,D).cpu().numpy()
         vol_f[i] = y
-    vol = fft.ihtn_center(vol_f*rnorm[1]+rnorm[0])
+    vol = fft.ihtn_center(vol_f*norm[1]+norm[0])
     return vol, vol_f
+
+def save_checkpoint(model, lattice, optim, epoch, norm, out_mrc, out_weights):
+    model.eval()
+    vol, vol_f = eval_volume(model, lattice, lattice.D, norm)
+    mrc.write(out_mrc, vol.astype(np.float32))
+    torch.save({
+        'norm': norm,
+        'epoch':epoch,
+        'model_state_dict':model.state_dict(),
+        'optimizer_state_dict':optim.state_dict(),
+        }, out_weights)
+
+def train(model, lattice, bnb, optim, batch, L, tilt=None):
+    y, yt = batch
+    # BNB inference of orientation 
+    model.eval()
+    with torch.no_grad():
+        rot = bnb.opt_theta(y, L, yt)
+    # Train model 
+    model.train()
+    optim.zero_grad()
+    y_recon = model(lattice.coords @ rot)
+    y_recon = y_recon.view(-1, lattice.D, lattice.D)
+    loss = F.mse_loss(y_recon,y)
+    if tilt is not None:
+        y_recon_tilt = model(lattice.coords @ tilt @ rot)
+        y_recon_tilt = y_recon_tilt.view(-1, lattice.D, lattice.D)
+        loss = .5*loss + .5*F.mse_loss(y_recon_tilt,yt)
+    loss.backward()
+    optim.step()
+    return loss.item()
 
 def main(args):
     log(args)
     t1 = dt.now()
-    if args.outdir is not None and not os.path.exists(args.outdir):
+    if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
 
     # set the random seed
@@ -84,42 +114,29 @@ def main(args):
 
     ## set the device
     use_cuda = torch.cuda.is_available()
+    device = torch.device('cuda' if use_cuda else 'cpu')
     log('Use cuda {}'.format(use_cuda))
     if use_cuda:
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
     # load the particles
-    particles, _, _ = mrc.parse_mrc(args.particles)
-    Nimg, ny, nx = particles.shape
-    assert nx == ny
-    nz = max(nx,ny)
-    log('Loaded {} {}x{} images'.format(Nimg, ny, nx))
-    particles = np.asarray([fft.ht2_center(img).astype(np.float32) for img in particles])
-    assert particles.shape == (Nimg,ny,nx)
-    rnorm  = [np.mean(particles), np.std(particles)]
-    log('Particle FT stack mean, std: {} +/- {}'.format(*rnorm))
-    rnorm[0] = 0
-    log('Normalizing FT by mean, std: {} +/- {}'.format(*rnorm))
-    particles = (particles - rnorm[0])/rnorm[1]
-
-    # load particles from tilt series
-    if args.tilt is not None:
-        particles_tilt, _, _ = mrc.parse_mrc(args.tilt)
-        assert particles_tilt.shape == (Nimg, ny, nx), 'Tilt series pair must have same dimensions as untilted particles'
-        log('Loaded {} {}x{} tilt series images'.format(Nimg, ny, nx))
-        particles_tilt = np.asarray([fft.ht2_center(img).astype(np.float32) for img in particles_tilt])
-        particles_tilt = (particles_tilt - rnorm[0])/rnorm[1]
-    
+    if args.tilt is None:
+        data = dataset.MRCData(args.particles)
+        tilt = None
+    else:
+        data = dataset.TiltMRCData(args.particles, args.tilt)
         theta = args.tilt_deg*np.pi/180
         tilt = np.array([[1.,0.,0.],
                         [0, np.cos(theta), -np.sin(theta)],
                         [0, np.sin(theta), np.cos(theta)]]).astype(np.float32)
         tilt = torch.tensor(tilt)
-    else:
-        tilt = None
+    log('Loaded {} {}x{} images'.format(data.N, data.D, data.D))
+    log('Normalized FT by {} +/- {}'.format(*data.norm))
+    D = data.D
+    Nimg = data.N
 
-    lattice = Lattice(nx)
-    model = FTSliceDecoder(3, nx, args.layers, args.dim, nn.ReLU)
+    lattice = Lattice(D)
+    model = FTSliceDecoder(3, D, args.layers, args.dim, nn.ReLU)
     bnb = BNNBHomo(model, lattice, tilt)
 
     optim = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
@@ -140,78 +157,38 @@ def main(args):
         Lsched = LinearSchedule(args.l_start,args.l_end,0,args.l_end_it)
 
     # training loop
-    num_epochs = args.num_epochs
-    for epoch in range(start_epoch, num_epochs):
+    data_generator = DataLoader(data, batch_size=args.batch_size, shuffle=True)
+    for epoch in range(start_epoch, args.num_epochs):
+        batch_it = 0
         loss_accum = 0
-        batch_it = 0 
-        num_batches = np.ceil(Nimg / args.batch_size).astype(int)
-        for minibatch_i in np.array_split(np.random.permutation(Nimg),num_batches):
-            batch_it += len(minibatch_i)
-            global_it = Nimg*epoch + batch_it
-
-            y = torch.from_numpy(particles[minibatch_i])
-            if use_cuda: y = y.cuda()
-            if tilt is not None:
-                yt = torch.from_numpy(particles_tilt[minibatch_i])
-                if use_cuda: yt = yt.cuda()
-            else: yt=None
-
-            # find the optimal orientation for each image
+        for batch in data_generator:
+            batch = (batch.to(device), None) if tilt is None else (batch[0].to(device), batch[1].to(device))
+            batch_it += len(batch[0])
+            global_it = Nimg*epoch+batch_it
             L = Lsched(global_it)
             if L: L = int(L)
-            model.eval()
-            with torch.no_grad():
-                rot = bnb.opt_theta(y,L,yt)
-            model.train()
-
-            # train the decoder
-            y_recon = model(lattice.coords @ rot)
-            y_recon = y_recon.view(-1, ny, nx)
-            loss = F.mse_loss(y_recon,y)
-
-            if tilt is not None:
-                y_recon_tilt = model(lattice.coords @ tilt @ rot)
-                y_recon_tilt = y_recon_tilt.view(-1, ny, nx)
-                loss = .5*loss + .5*F.mse_loss(y_recon_tilt,yt)
-
-            loss.backward()
-            optim.step()
-            optim.zero_grad()
             
+            # train the model
+            loss_item = train(model, lattice, bnb, optim, batch, L, tilt)
+           
             # logging
-            loss_accum += loss.item()*len(minibatch_i)
-
+            loss_accum += loss_item*len(batch[0])
             if batch_it % args.log_interval == 0:
-                log('# [Train Epoch: {}/{}] [{}/{} images] loss={:.4f}'.format(epoch+1, num_epochs, batch_it, Nimg, loss.item()))
+                log('# [Train Epoch: {}/{}] [{}/{} images] loss={:.4f}'.format(epoch+1, args.num_epochs, batch_it, Nimg, loss_item))
         log('# =====> Epoch: {} Average loss = {:.4}'.format(epoch+1, loss_accum/Nimg))
 
         if args.checkpoint and epoch % args.checkpoint == 0:
-            model.eval()
-            vol, vol_f = eval_volume(model, lattice, nx, rnorm)
-            mrc.write('{}/reconstruct.{}.mrc'.format(args.outdir,epoch), vol.astype(np.float32))
-            path = '{}/weights.{}.pkl'.format(args.outdir,epoch)
-            torch.save({
-                'norm': rnorm,
-                'epoch':epoch,
-                'model_state_dict':model.state_dict(),
-                'optimizer_state_dict':optim.state_dict(),
-                }, path)
-            model.train()
+            out_mrc = '{}/reconstruct.{}.mrc'.format(args.outdir,epoch)
+            out_weights = '{}/weights.{}.pkl'.format(args.outdir,epoch)
+            save_checkpoint(model, lattice, optim, epoch, data.norm, out_mrc, out_weights)
 
     ## save model weights and evaluate the model on 3D lattice
-    model.eval()
-    vol, vol_f = eval_volume(model, lattice, nx, rnorm)
-    mrc.write('{}/reconstruct.mrc'.format(args.outdir), vol.astype(np.float32))
-    path = '{}/weights.pkl'.format(args.outdir)
-    torch.save({
-        'norm': rnorm,
-        'epoch':epoch,
-        'model_state_dict':model.state_dict(),
-        'optimizer_state_dict':optim.state_dict(),
-        }, path)
-    
+    out_mrc = '{}/reconstruct.mrc'.format(args.outdir)
+    out_weights = '{}/weights.pkl'.format(args.outdir)
+    save_checkpoint(model, lattice, optim, epoch, data.norm, out_mrc, out_weights)
+   
     td = dt.now()-t1
-    log('Finsihed in {} ({} per epoch)'.format(td, td/num_epochs))
+    log('Finsihed in {} ({} per epoch)'.format(td, td/args.num_epochs))
 
 if __name__ == '__main__':
     args = parse_args().parse_args()

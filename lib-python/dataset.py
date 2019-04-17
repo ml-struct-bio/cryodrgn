93e7e9cc6558ed11a5e2ebb386e9d5833a3c554c
Ellen Zhong <ellen.zhong@virginia.edu>
2019-04-17 11:38:01 -0400

    New classes MRCData and TiltMRCData subclassing torch.utils.data.Dataset

---
+++ lib-python/dataset.py
@@ -0,0 +1,77 @@
+import numpy as np
+import torch
+from torch.utils import data
+
+import fft
+import mrc
+
+class LazyMRCData(data.Dataset):
+    def __init__(self, *args):
+        raise NotImplementedError
+
+class MRCData(data.Dataset):
+    '''
+    Class representing an .mrcs stack file
+    '''
+    def __init__(self, mrcfile, norm=None, keepreal=False):
+        particles_real, _, _ = mrc.parse_mrc(mrcfile)
+        N, ny, nx = particles_real.shape
+        assert ny == nx, "Images must be square"
+        assert ny % 2 == 0, "Image size must be even"
+        # compute FT
+        particles = np.asarray([fft.ht2_center(img).astype(np.float32) for img in particles_real])
+        # normalize
+        if norm is None:
+            norm  = [np.mean(particles), np.std(particles)]
+            norm[0] = 0
+        particles = (particles - norm[0])/norm[1]
+        self.particles = particles
+        self.N = N
+        self.D = ny
+        self.norm = norm
+        if keepreal:
+            self.particles_real = particles_real
+
+    def __len__(self):
+        return self.N
+
+    def __getitem__(self, index):
+        return self.particles[index]
+
+class TiltMRCData(data.Dataset):
+    '''
+    Class representing an .mrcs tilt series pair
+    '''
+
+    def __init__(self, mrcfile, mrcfile_tilt, norm=None):
+        particles, _, _ = mrc.parse_mrc(mrcfile)
+        N, ny, nx = particles.shape
+        assert ny == nx, "Images must be square"
+        assert ny % 2 == 0, "Image size must be even"
+        particles_tilt, _, _ = mrc.parse_mrc(mrcfile_tilt)
+        assert particles_tilt.shape == (N, ny, nx), "Tilt series pair must have same dimensions as untilted particles"
+        # compute FT
+        particles = np.asarray([fft.ht2_center(img).astype(np.float32) for img in particles])
+        particles_tilt = np.asarray([fft.ht2_center(img).astype(np.float32) for img in particles_tilt])
+        # normalize
+        if norm is None:
+            norm  = [np.mean(particles), np.std(particles)]
+            norm[0] = 0
+        particles = (particles - norm[0])/norm[1]
+        particles_tilt = (particles_tilt - norm[0])/norm[1]
+        self.particles = particles
+        self.particles_tilt = particles_tilt
+        self.norm = norm
+        self.N = N
+        self.D = ny
+
+    def __len__(self):
+        return self.N
+
+    def __getitem__(self, index):
+        return self.particles[index], self.particles_tilt[index]
+
+
+
+   
+

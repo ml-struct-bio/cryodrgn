8294a53f45a383f2b5170ea2d1336e6703252665
Ellen Zhong <zhonge@mit.edu>
2019-04-25 14:01:02 -0400

    Use odd grid size for shifts so (0,0) is in base grid

--- lib-python/shift_grid.py
+++ lib-python/shift_grid.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def grid_1d(resol, extent):
-    Npix = 6*2**resol
+    Npix = 7*2**resol
     dt = 2*extent/Npix
     grid = np.arange(Npix, dtype=np.float32)*dt + dt/2 - extent
     return grid
@@ -19,14 +19,14 @@ def base_shift_grid(extent):
 ### Neighbor Finding ###
 
 def get_1d_neighbor(mini, curr_res, extent):
-    Npix = 6*2**(curr_res+1)
+    Npix = 7*2**(curr_res+1)
     dt = 2*extent/Npix
     ind = np.array([2*mini, 2*mini+1], dtype=np.float32)
     return dt*ind + dt/2 - extent, ind
 
 def get_base_ind(ind):
-    xi = ind % 6
-    yi = ind // 6 # integer division
+    xi = ind % 7
+    yi = ind // 7 # integer division
     return xi, yi
 
 def get_neighbor(xi, yi, curr_res, extent):

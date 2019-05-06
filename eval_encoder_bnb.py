edeb35f1f436e78b945c844209d9496aae46961e
Ellen Zhong <zhonge@mit.edu>
2019-05-05 21:24:52 -0400

    Add option --tilt to eval_encoder_bnb.py and remove obsolete eval_encoder_tilt.py

--- eval_encoder_bnb.py
+++ eval_encoder_bnb.py
@@ -23,19 +23,18 @@ vlog = utils.vlog
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('particles', help='Particle stack file (.mrc)')
+    parser.add_argument('particles', help='Particle stack file (.mrcs)')
     parser.add_argument('weights', help='Model weights')
-    parser.add_argument('-N', type=int, help='First N images (default: all images)')
+    parser.add_argument('--tilt', help='Particle stack file for tilt series (.mrcs')
     parser.add_argument('-o', type=os.path.abspath, required=True, help='Output pickle')
     parser.add_argument('-v','--verbose',action='store_true',help='Increaes verbosity')
-    parser.add_argument('--ref',type=int, default=0,help='Reference image to align by')
 
     group = parser.add_argument_group('Architecture parameters')
     group.add_argument('-b','--batch-size', type=int, default=100, help='Minibatch size (default: %(default)s)')
     group.add_argument('--qlayers', type=int, default=10, help='Number of hidden layers (default: %(default)s)')
     group.add_argument('--qdim', type=int, default=128, help='Number of nodes in hidden layers (default: %(default)s)')
     group.add_argument('--zdim', type=int, default=1, help='Dimension of latent variable')
-    group.add_argument('--encode-mode', default='resid', choices=('conv','resid','mlp'), help='Type of encoder network (default: %(default)s)')
+    group.add_argument('--encode-mode', default='resid', choices=('conv','resid','mlp','tilt'), help='Type of encoder network (default: %(default)s)')
     group.add_argument('--players', type=int, default=10, help='Number of hidden layers (default: %(default)s)')
     group.add_argument('--pdim', type=int, default=128, help='Number of nodes in hidden layers (default: %(default)s)')
     return parser
@@ -52,7 +51,11 @@ def main(args):
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
     # load the particles
-    data = dataset.MRCData(args.particles)
+    if args.tilt:
+        data = dataset.TiltMRCData(args.particles,args.tilt)
+    else:
+        data = dataset.MRCData(args.particles)
+
     Nimg, D = data.N, data.D
     lattice = Lattice(D)
     model = HetOnlyVAE(lattice, D*D, args.qlayers, args.qdim, args.players, args.pdim,
@@ -66,9 +69,13 @@ def main(args):
     z_all = []
     data_iterator = DataLoader(data, batch_size=args.batch_size, shuffle=False)
     for batch in data_iterator:
+        B = len(batch[0])
         y = batch[0].to(device)
-        B = len(y)
-        mu, logvar = model.encode(y.view(B,-1)) 
+        if args.tilt:
+            yt = batch[0].to(device)
+            mu, logvar = model.encode((y.view(B,-1), yt.view(B,-1)))
+        else:
+            mu, logvar = model.encode(y.view(B,-1))
         z_all.append(mu.detach().cpu().numpy())
 
     if args.o:

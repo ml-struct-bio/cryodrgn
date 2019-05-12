44134ea167ef6b7372589f1ffaffd5f281a7764d
Ellen Zhong <zhonge@mit.edu>
2019-05-12 17:54:27 -0400

    Add new script compare_rot.py

---
+++ utils/compare_rot.py
@@ -0,0 +1,65 @@
+'''Align and compute distance between two series of rotation matrices'''
+
+import argparse
+import numpy as np
+import sys, os
+sys.path.insert(0,'{}/../lib-python'.format(os.path.dirname(os.path.abspath(__file__))))
+import utils
+log = utils.log
+import matplotlib.pyplot as plt
+from scipy.linalg import logm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('rot1', help='Input')
+    parser.add_argument('rot2', help='Input rotations')
+    parser.add_argument('-i', type=int, help='index to align on')
+    parser.add_argument('--flip',action='store_true',help='Flip hand')
+    parser.add_argument('-o', help='Output')
+    return parser
+
+def geodesic_so3(A,B):
+    return np.sum(logm(np.dot(A.T,B))**2)**.5
+
+def fast_dist(a,b):
+    return np.sum((a-b)**2)
+
+def align_rot(rotA,rotB,i,flip=False):
+    if flip:
+        x = -1*np.ones((3,3))
+        x[2] = 1
+        rotB = [x*r for r in rotB] # left multiplication of [-1,-1,1] diag matrix
+                                    
+    ref = rotA[i]
+    rot = np.array([np.dot(x, ref.T) for x in rotA])
+    ref = rotB[i]
+    rot_hat = np.array([np.dot(x, ref.T) for x in rotB])
+    return rot, rot_hat
+
+def align_rot2(rotA,rotB,i,flip=False):
+    if flip:
+        x = -1*np.ones((3,3))
+        x[2] = 1
+        rotB = [x*r for r in rotB] # left multiplication of [-1,-1,1] diag matrix
+    ref = np.dot(rotB[i].T, rotA[i])
+    print(ref)
+    rot_hat = np.array([np.dot(x, ref) for x in rotB])
+    return rot_hat
+
+def main(args):
+    rot1 = utils.load_pkl(args.rot1)
+    #rot1 = np.array([utils.R_from_eman(*x) for x in rot1])
+    rot2 = utils.load_pkl(args.rot2)
+    assert rot1.shape == rot2.shape
+
+    rot2 = align_rot2(rot1,rot2,args.i,args.flip)
+    dists = np.asarray([fast_dist(a,b) for a,b in zip(rot1, rot2)])
+
+    log('Mean error: {}'.format(np.mean(dists)))
+    log('Median error: {}'.format(np.median(dists)))
+    plt.hist(dists)
+    plt.show()
+
+if __name__ == '__main__':
+    main(parse_args().parse_args())

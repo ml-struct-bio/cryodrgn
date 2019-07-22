9ed043c37e5f4eba0960882cbc0a044db0fe7af5
Ellen Zhong <zhonge@mit.edu>
2019-07-22 11:18:34 -0400

    Add fftn_center to fft.py

--- lib-python/fft.py
+++ lib-python/fft.py
@@ -3,6 +3,9 @@ import numpy as np
 def fft2_center(img):
     return np.fft.fftshift(np.fft.fft2(np.fft.fftshift(img,axes=(-1,-2))),axes=(-1,-2))
 
+def fftn_center(img):
+    return np.fft.fftshift(np.fft.fftn(np.fft.fftshift(img)))
+
 def ifftn_center(V):
     V = np.fft.ifftshift(V)
     V = np.fft.ifftn(V)

7b6d0bcb19582397fd54d5ad5d26680f5912f825
Ellen Zhong <zhonge@mit.edu>
2019-08-15 11:13:28 -0400

    Add --enc-type argument to eval_decoder.py

--- eval_decoder.py
+++ eval_decoder.py
@@ -47,6 +47,7 @@ def parse_args():
     group.add_argument('--players', type=int, default=10, help='Number of hidden layers (default: %(default)s)')
     group.add_argument('--pdim', type=int, default=128, help='Number of nodes in hidden layers (default: %(default)s)')
     group.add_argument('--enc-mask', type=int, help='Circulask mask of image for encoder')
+    group.add_argument('--enc-type', choices=('geom_ft','geom_full','geom_lowf','geom_nohighf','linear_lowf','none'), default='linear_lowf', help='Type of positional encoding')
 
     return parser
 
@@ -66,7 +67,7 @@ def main(args):
     lattice = Lattice(D, extent=args.l_extent)
     if args.enc_mask: args.enc_mask = lattice.get_circular_mask(args.enc_mask)
     model = HetOnlyVAE(lattice, args.qlayers, args.qdim, args.players, args.pdim,
-                args.zdim, encode_mode=args.encode_mode, enc_mask=args.enc_mask)
+                args.zdim, encode_mode=args.encode_mode, enc_mask=args.enc_mask, enc_type=args.enc_type)
 
     log('Loading weights from {}'.format(args.weights))
     checkpoint = torch.load(args.weights)

02e66dbf606fb05e35354145cc913c530e4e8206
Ellen Zhong <zhonge@mit.edu>
2019-10-02 18:10:30 -0400

    New particle preprocessing - window dataset in real space

--- lib-python/dataset.py
+++ lib-python/dataset.py
@@ -12,7 +12,7 @@ class LazyMRCData(data.Dataset):
     '''
     Class representing an .mrcs stack file -- images loaded on the fly
     '''
-    def __init__(self, mrcfile, norm=None, keepreal=False, invert_data=False):
+    def __init__(self, mrcfile, norm=None, keepreal=False, invert_data=False, window=True):
         assert not keepreal, 'Not implemented error'
         if mrcfile.endswith('.txt'):
             particles = mrc.parse_mrc_list(mrcfile, lazy=True)
@@ -30,6 +30,7 @@ class LazyMRCData(data.Dataset):
         if norm is None:
             norm = self.estimate_normalization()
         self.norm = norm
+        self.window = window_mask(ny, .85, .99) if self.window else None
 
     def estimate_normalization(self, n=1000):
         n = min(n,self.N)
@@ -43,6 +44,8 @@ class LazyMRCData(data.Dataset):
 
     def get(self, i):
         img = self.particles[i].get()
+        if self.window is not None:
+            img *= self.window
         img = fft.ht2_center(img).astype(np.float32)
         if self.invert_data: img *= -1
         img = fft.symmetrize_ht(img)
@@ -56,11 +59,19 @@ class LazyMRCData(data.Dataset):
         return self.get(index), index
 
 
+def window_mask(D, in_rad, out_rad):
+    assert D % 2 == 0
+    x0, x1 = np.meshgrid(np.linspace(-1, 1, D, endpoint=False, dtype=np.float32), 
+                         np.linspace(-1, 1, D, endpoint=False, dtype=np.float32))
+    r = (x0**2 + x1**2)**.5
+    mask = np.minimum(1.0, np.maximum(0.0, 1 - (r-in_rad)/(out_rad-in_rad)))
+    return mask
+
 class MRCData(data.Dataset):
     '''
     Class representing an .mrcs stack file
     '''
-    def __init__(self, mrcfile, norm=None, keepreal=False, invert_data=False, ind=None):
+    def __init__(self, mrcfile, norm=None, keepreal=False, invert_data=False, ind=None, window=True):
         if mrcfile.endswith('.txt'):
             particles_real = mrc.parse_mrc_list(mrcfile)
         else:
@@ -71,6 +82,11 @@ class MRCData(data.Dataset):
         assert ny == nx, "Images must be square"
         assert ny % 2 == 0, "Image size must be even"
         log('Loaded {} {}x{} images'.format(N, ny, nx))
+
+        # Real space window
+        if window:
+            particles_real *= window_mask(ny, .85, .99)
+
         # compute HT
         particles = np.asarray([fft.ht2_center(img) for img in particles_real])
         particles = particles.astype(np.float32)
@@ -104,28 +120,34 @@ class TiltMRCData(data.Dataset):
     '''
     Class representing an .mrcs tilt series pair
     '''
-    def __init__(self, mrcfile, mrcfile_tilt, norm=None, keepreal=False, invert_data=False, ind=None):
+    def __init__(self, mrcfile, mrcfile_tilt, norm=None, keepreal=False, invert_data=False, ind=None, window=True):
         if mrcfile.endswith('.txt'):
             particles_real = mrc.parse_mrc_list(mrcfile)
         else:
             particles_real, _, _ = mrc.parse_mrc(mrcfile)
         if mrcfile_tilt.endswith('.txt'):
-            particles_tilt = mrc.parse_mrc_list(mrcfile_tilt)
+            particles_tilt_real = mrc.parse_mrc_list(mrcfile_tilt)
         else:
-            particles_tilt, _, _ = mrc.parse_mrc(mrcfile_tilt)
+            particles_tilt_real, _, _ = mrc.parse_mrc(mrcfile_tilt)
         if ind is not None:
             particles_real = particles_real[ind]
-            particles_tilt = particles_tilt[ind]
+            particles_tilt_real = particles_tilt_real[ind]
         N, ny, nx = particles_real.shape
         assert ny == nx, "Images must be square"
         assert ny % 2 == 0, "Image size must be even"
         log('Loaded {} {}x{} images'.format(N, ny, nx))
-        assert particles_tilt.shape == (N, ny, nx), "Tilt series pair must have same dimensions as untilted particles"
+        assert particles_tilt_real.shape == (N, ny, nx), "Tilt series pair must have same dimensions as untilted particles"
         log('Loaded {} {}x{} tilt pair images'.format(N, ny, nx))
+
+        # Real space window
+        if window:
+            m = window_mask(ny, .85, .99)
+            particles_real *= m
+            particles_tilt_real *= m 
 
         # compute HT
         particles = np.asarray([fft.ht2_center(img) for img in particles_real]).astype(np.float32)
-        particles_tilt = np.asarray([fft.ht2_center(img) for img in particles_tilt]).astype(np.float32)
+        particles_tilt = np.asarray([fft.ht2_center(img) for img in particles_tilt_real]).astype(np.float32)
         if invert_data: 
             particles *= -1
             particles_tilt *= -1
@@ -150,6 +172,7 @@ class TiltMRCData(data.Dataset):
         self.keepreal = keepreal
         if keepreal:
             self.particles_real = particles_real
+            self.particles_tilt_real = particles_tilt_real
 
     def __len__(self):
         return self.N

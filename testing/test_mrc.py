8e6ece18b6cf9b4040f58ea6d8cc3eafd906c04c
Ellen Zhong <zhonge@mit.edu>
2019-11-25 13:56:36 -0500

    Update testing for mrc parsing to include .star and .txt file parsing

--- testing/test_mrc.py
+++ testing/test_mrc.py
@@ -8,4 +8,15 @@ data, _, _ = mrc.parse_mrc('{}/data/toy_projections.mrcs'.format(DIR), lazy=True
 data2, _, _ = mrc.parse_mrc('{}/data/toy_projections.mrcs'.format(DIR), lazy=False)
 data1=np.asarray([x.get() for x in data])
 assert (data1==data2).all()
+print('ok')
+
+import dataset
+data2 = dataset.load_particles('{}/data/toy_projections.star'.format(DIR))
+assert (data1==data2).all()
+print('ok')
+
+data2 = dataset.load_particles('{}/data/toy_projections.txt'.format(DIR))
+assert (data1==data2).all()
+print('ok')
+
 print('all ok')

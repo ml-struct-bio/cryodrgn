766a69e7539d95dd33e36126bad2e405ac89c9f5
Ellen Zhong <zhonge@mit.edu>
2019-12-19 13:45:51 -0500

    Update unittest for CTF parsing

--- testing/unittest.sh
+++ testing/unittest.sh
@@ -23,8 +23,13 @@ python ../backproject_nn.py data/toy_projections.mrcs --poses data/toy_angles.pk
 python ../backproject_voxel.py data/hand.mrcs --poses data/hand_rot.pkl -o output/backproject.mrc
 python ../backproject_voxel.py data/hand.mrcs --poses data/hand_rot.pkl -o output/backproject_tilt.mrc --tilt data/hand_tilt.mrcs
 
-python ../vae_het.py  data/toy_projections.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/toy_angles.pkl
-python ../vae_het.py  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl 
-python ../vae_het.py  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl --encode-mode conv
+python ../vae_het.py  data/toy_projections.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/toy_angles.pkl --zdim 10
+python ../vae_het.py  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl --zdim 10 
+python ../vae_het.py  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl --encode-mode conv --zdim 10
 python ../backproject_nn.py data/hand.mrcs --poses data/hand_rot.pkl -o output/hand_recon -b 8
 python ../backproject_nn.py data/hand.mrcs --poses data/hand_rot.pkl -o output/hand_recon --amp -b 8
+
+# CTF
+python ../utils/parse_ctf_csparc.py data/cryosparc_P12_J24_001_particles.cs -o test_ctf.pkl
+python ../utils/parse_ctf_star.py data/toy_projections.star -N 1000 --Apix 1 -o test_ctf.pkl
+python ../vae_het.py  data/toy_projections.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/toy_angles.pkl --ctf test_ctf.pkl --zdim 10

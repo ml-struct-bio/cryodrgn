f3bc97ddbdaf34d19e0765f31c669937152b3236
Ellen Zhong <zhonge@mit.edu>
2020-03-19 14:14:10 -0400

    New script to add pixel size to the .mrc header

---
+++ utils/add_psize.py
@@ -0,0 +1,33 @@
+'''Add pixel size to header of .mrc file'''
+
+import argparse
+import numpy as np
+import sys, os
+import pickle
+
+sys.path.insert(0,'{}/../lib-python'.format(os.path.dirname(os.path.abspath(__file__))))
+import utils
+import mrc
+log = utils.log 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('input', help='Input volume')
+    parser.add_argument('--apix', type=float, default=1, help='Angstrom/pixel (default: %(default)s)')
+    parser.add_argument('--flip', action='store_true', help='Flip handedness')
+    parser.add_argument('--invert', action='store_true', help='Invert volume')
+    parser.add_argument('-o', help='Output volume')
+    return parser
+
+def main(args):
+    x, _, _ = mrc.parse_mrc(args.input)
+    D = args.apix
+    if args.invert:
+        x *= -1
+    if args.flip:
+        x = x[::-1]
+    mrc.write(args.o, x, ax=D, ay=D, az=D)
+    log(f'Wrote {args.o}')
+
+if __name__ == '__main__':
+    main(parse_args().parse_args())

4a38562af92aaf66eb3ea2e5a0bfa39f4d1de34e
Ellen Zhong <zhonge@mit.edu>
2020-04-13 20:57:11 -0400

    Update quicktest.sh to use relative paths

--- testing/quicktest.sh
+++ testing/quicktest.sh
@@ -1,4 +1,4 @@
 set -e
 set -x
 
-python $CDRGN_SRC/vae_het.py  $CDRGN_SRC/testing/data/hand.mrcs -o /tmp/toy_recon_vae --lr .0001 --seed 0 --poses $CDRGN_SRC/testing/data/hand_rot.pkl --zdim 10 
+python ../vae_het.py  data/hand.mrcs -o /tmp/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl --zdim 10 

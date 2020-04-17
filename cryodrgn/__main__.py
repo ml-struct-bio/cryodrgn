18d9a80d4542e9664f6fd57bc6d303099699c713
Ellen Zhong <zhonge@mit.edu>
2020-04-17 10:28:19 -0400

    Add module docstring in the --help description

--- cryodrgn/__main__.py
+++ cryodrgn/__main__.py
@@ -29,14 +29,14 @@ def main():
         cryodrgn.commands.eval_images,
         ]
 
-    subparsers = parser.add_subparsers(title='commands')#, metavar='<command>')
+    subparsers = parser.add_subparsers(title='Choose a command')
     subparsers.required = 'True'
 
     def get_str_name(module):
         return os.path.splitext(os.path.basename(module.__file__))[0]
 
     for module in modules:
-        this_parser = subparsers.add_parser(get_str_name(module))
+        this_parser = subparsers.add_parser(get_str_name(module), description=module.__doc__)
         module.add_args(this_parser)
         this_parser.set_defaults(func=module.main)
 

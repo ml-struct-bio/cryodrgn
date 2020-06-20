8cbe029602cda7b36246350bf6aa26309648d8ed
Ellen Zhong <zhonge@mit.edu>
2020-06-20 00:53:35 -0400

    Split into separate scripts

--- utils/invert_contrast.py
+++ utils/invert_contrast.py
@@ -1,4 +1,4 @@
-'''Add pixel size to header of .mrc file'''
+'''Invert the contrast of an .mrc file'''
 
 import argparse
 import numpy as np
@@ -12,7 +12,6 @@ log = utils.log
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('input', help='Input volume (.mrc)')
-    parser.add_argument('--Apix', type=float, default=1, help='Angstrom/pixel (default: %(default)s)')
     parser.add_argument('-o', help='Output volume (.mrc)')
     return parser
 
@@ -20,7 +19,7 @@ def main(args):
     assert args.input.endswith('.mrc'), "Input volume must be .mrc file"
     assert args.o.endswith('.mrc'), "Output volume must be .mrc file"
     x, h = mrc.parse_mrc(args.input)
-    h.update_apix(args.Apix)
+    x *= -1
     mrc.write(args.o, x, header=h)
     log(f'Wrote {args.o}')
 

3c8b5ae3e525e0b3825646a1445a94378e2fa797
Ellen Zhong <zhonge@mit.edu>
2020-08-02 11:54:10 -0400

    Add more logging

--- utils/filter_pkl.py
+++ utils/filter_pkl.py
@@ -4,12 +4,13 @@ import argparse
 import numpy as np
 import sys, os
 import pickle
+log = print
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('input', help='Input data .pkl')
     parser.add_argument('--ind', required=True, help='Selected indices array (.pkl)')
-    parser.add_argument('-o', help='Output data .pkl')
+    parser.add_argument('-o', type=os.path.abspath, help='Output data .pkl')
     return parser
 
 def load_pkl(x):
@@ -21,17 +22,18 @@ def main(args):
 
     # pose.pkl contains a tuple of rotations and translations
     if type(x) == tuple:
-        print([xx.shape for xx in x])
+        log('Detected pose.pkl')
+        log(f'Old shape: {[xx.shape for xx in x]}')
         x = (xx[ind] for xx in x)
         x = tuple(x)
-        print([xx.shape for xx in x])
+        log(f'New shape: {[xx.shape for xx in x]}')
 
     # all other cryodrgn pkls 
     else:
-        print(x.shape)
+        log(f'Old shape: {x.shape}')
         x = x[ind]
-        print(x.shape)
-    print(f'Saving {args.o}')
+        log(f'New shape: {x.shape}')
+    log(f'Saving {args.o}')
     pickle.dump(x, open(args.o,'wb'))
 
 if __name__ == '__main__':

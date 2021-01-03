113babe71f206741b740852ae328c7d87f5ccfd0
Ellen Zhong <zhonge@mit.edu>
2021-01-02 19:06:29 -1000

    Update testing script

--- testing/test_mrc.py
+++ testing/test_mrc.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 import sys, os
-DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0,'{}/../lib-python'.format(DIR))
-import mrc
+from cryodrgn import mrc
 import numpy as np
 data, _ = mrc.parse_mrc('{}/data/toy_projections.mrcs'.format(DIR), lazy=True)
 data2, _ = mrc.parse_mrc('{}/data/toy_projections.mrcs'.format(DIR), lazy=False)
@@ -10,7 +8,7 @@ data1=np.asarray([x.get() for x in data])
 assert (data1==data2).all()
 print('ok')
 
-import dataset
+from cryodrgn import dataset
 data2 = dataset.load_particles('{}/data/toy_projections.star'.format(DIR))
 assert (data1==data2).all()
 print('ok')

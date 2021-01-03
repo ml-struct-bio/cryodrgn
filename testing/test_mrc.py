879b4227e9e714c3b0f153026f80f03d7c7131e9
Ellen Zhong <zhonge@mit.edu>
2021-01-03 00:23:03 -0500

    Update testing script

--- testing/test_mrc.py
+++ testing/test_mrc.py
@@ -2,18 +2,18 @@
 import sys, os
 from cryodrgn import mrc
 import numpy as np
-data, _ = mrc.parse_mrc('{}/data/toy_projections.mrcs'.format(DIR), lazy=True)
-data2, _ = mrc.parse_mrc('{}/data/toy_projections.mrcs'.format(DIR), lazy=False)
+data, _ = mrc.parse_mrc('data/toy_projections.mrcs', lazy=True)
+data2, _ = mrc.parse_mrc('data/toy_projections.mrcs', lazy=False)
 data1=np.asarray([x.get() for x in data])
 assert (data1==data2).all()
 print('ok')
 
 from cryodrgn import dataset
-data2 = dataset.load_particles('{}/data/toy_projections.star'.format(DIR))
+data2 = dataset.load_particles('data/toy_projections.star')
 assert (data1==data2).all()
 print('ok')
 
-data2 = dataset.load_particles('{}/data/toy_projections.txt'.format(DIR))
+data2 = dataset.load_particles('data/toy_projections.txt')
 assert (data1==data2).all()
 print('ok')
 

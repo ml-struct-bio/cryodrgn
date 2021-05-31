f788eec6899cc42b41629b86a72d5c142141c9be
Ellen Zhong <zhonge@mit.edu>
2021-05-31 08:55:18 -0400

    Add arugment --invert to view_stack.py

--- utils/view_stack.py
+++ utils/view_stack.py
@@ -14,6 +14,7 @@ from cryodrgn import analysis
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('input', help='Particle stack')
+    parser.add_argument('--invert', action='store_true')
     parser.add_argument('-o', help='Output PNG')
     return parser
 
@@ -21,6 +22,8 @@ def main(args):
     stack, _ = mrc.parse_mrc(args.input,lazy=True)
     print('{} {}x{} images'.format(len(stack), *stack[0].get().shape))
     stack = [stack[x].get() for x in range(9)]
+    if args.invert:
+        stack = [-1*x for x in stack]
     analysis.plot_projections(stack)
     if args.o:
         plt.savefig(args.o)

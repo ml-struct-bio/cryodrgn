d95a2578a0147d89396644a206bf699b772ef512
Ellen Zhong <zhonge@mit.edu>
2021-05-31 07:28:27 -0400

    New script that preprocesses input mrcs files into format for cryoDRGN training
    Preprocessing steps include:
    * windowing images in real space
    * inverting data sign
    * computing FFT (technically HT)
    * downsampling (optional)
    * symmetrizing HT (DxD -> (D+1)x(D+1) image)
    
    Loads and saves the data in chunks to avoid OOM issues. Also uses multiprocessing
    to parallelize FFT computation.
    
    This is usually done after loading the data in cryodrgn train_vae (to avoid storing
    multiple copies of the data). We now modularize this step since data preprocessing
    is the memory bottleneck for training large datasets and to set up the inputs
    in a standard format for chunked data loading.

---
+++ cryodrgn/commands/preprocess_mrcs.py
@@ -0,0 +1,115 @@
+'''
+Preprocess a dataset for more streamlined training
+'''
+
+import argparse
+import numpy as np
+import sys, os
+import math
+import multiprocessing as mp
+from multiprocessing import Pool
+
+from cryodrgn import utils
+from cryodrgn import mrc
+from cryodrgn import fft
+from cryodrgn import dataset
+
+log = utils.log
+
+def add_args(parser):
+    parser.add_argument('mrcs', help='Input particles or volume (.mrc, .mrcs, .star, or .txt)')
+    parser.add_argument('-D', type=int, help='New box size in pixels (if downsampling), must be even')
+    parser.add_argument('-o', metavar='MRCS', type=os.path.abspath, required=True, help='Output projection stack (.mrcs)')
+    parser.add_argument('-b', type=int, default=5000, help='Batch size for processing images (default: %(default)s)')
+    parser.add_argument('--chunk', type=int, default=100000, help='Chunksize (in # of images) to split particle stack when saving')
+    parser.add_argument('--no-window', dest='window', action='store_false', help='Flag for relion3.1 star format')
+    parser.add_argument('--uninvert-data', dest='invert_data', action='store_false', help='Flag for relion3.1 star format')
+    parser.add_argument('--relion31', action='store_true', help='Flag for relion3.1 star format')
+    parser.add_argument('--datadir', help='Optionally provide path to input .mrcs if loading from a .star or .cs file')
+    parser.add_argument('--max-threads', type=int, default=16, help='Maximum number of CPU cores for parallelization (default: %(default)s)')
+    return parser
+
+def mkbasedir(out):
+    if not os.path.exists(os.path.dirname(out)):
+        os.makedirs(os.path.dirname(out))
+
+def warnexists(out):
+    if os.path.exists(out):
+        log(f'Warning: {out} already exists. Overwriting.')
+
+def main(args):
+    mkbasedir(args.o)
+    warnexists(args.o)
+    assert (args.o.endswith('.mrcs') or args.o.endswith('.txt')), "Must specify output in .mrcs file format"
+
+    lazy = True
+
+    images = dataset.load_particles(args.mrcs, lazy=lazy, datadir=args.datadir, relion31=args.relion31)
+
+    D = images[0].get().shape[0] if lazy else images.shape[-1]
+    window = args.window
+    invert_data = args.invert_data
+    downsample = (args.D and args.D < D)
+    if downsample:
+        assert args.D <= D, f'New box size {args.D} cannot be larger than the original box size {D}'
+        assert args.D % 2 == 0, 'New box size must be even'
+        start = int(D/2 - args.D/2)
+        stop = int(D/2 + args.D/2)
+        D = args.D
+
+    def _combine_imgs(imgs):
+        ret = []
+        for img in imgs:
+            img.shape = (1,*img.shape) # (D,D) -> (1,D,D)
+        cur = imgs[0]
+        for img in imgs[1:]:
+            if img.fname == cur.fname and img.offset == cur.offset + 4*np.product(cur.shape):
+                cur.shape = (cur.shape[0] + 1, *cur.shape[1:])
+            else:
+                ret.append(cur)
+                cur = img
+        ret.append(cur)
+        return ret
+
+    def preprocess(imgs):
+        if lazy:
+            imgs = _combine_imgs(imgs)
+            imgs = np.concatenate([i.get() for i in imgs])
+        with Pool(min(args.max_threads, mp.cpu_count())) as p:
+            # todo: refactor as a routine in dataset.py
+            if window:
+                imgs *= dataset.window_mask(D, .85, .99)
+            ret = np.asarray(p.map(fft.ht2_center, imgs))
+            if invert_data:
+                ret *= -1
+            if downsample:
+                ret = ret[:, start:stop, start:stop]
+            ret = fft.symmetrize_ht(ret)
+        return ret
+
+    def preprocess_in_batches(imgs, b):
+        ret = np.empty((len(imgs), D+1, D+1), dtype=np.float32)
+        for ii in range(math.ceil(len(imgs)/b)):
+            log(f'Processing batch {ii}')
+            ret[ii*b:(ii+1)*b,:,:] = preprocess(imgs[ii*b:(ii+1)*b])
+        return ret
+
+    nchunks = math.ceil(len(images)/args.chunk)
+    out_mrcs = [f'.{i}.ft'.join(os.path.splitext(args.o)) for i in range(nchunks)]
+    chunk_names = [os.path.basename(x) for x in out_mrcs]
+    for i in range(nchunks):
+        log(f'Processing chunk {i}')
+        chunk = images[i*args.chunk:(i+1)*args.chunk]
+        new = preprocess_in_batches(chunk, args.b)
+        log(new.shape)
+        log(f'Saving {out_mrcs[i]}')
+        mrc.write(out_mrcs[i], new, is_vol=False)
+
+    out_txt = f'{os.path.splitext(args.o)[0]}.ft.txt'
+    log(f'Saving {out_txt}')
+    with open(out_txt,'w') as f:
+        f.write('\n'.join(chunk_names))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    main(add_args(parser).parse_args())

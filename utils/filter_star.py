8a4b843bd9a15de5bc4199b8b352e9bce5727d6b
Barrett Powell <bmp@mit.edu>
2021-07-16 13:10:37 -0400

    filter_star.py works with RELION 3.1 star files using new Starfile.df_optics

--- utils/filter_star.py
+++ utils/filter_star.py
@@ -1,7 +1,6 @@
 '''
 Filter a .star file
 
-(Note: RELION 3.1 star files are not supported)
 '''
 
 import argparse
@@ -18,16 +17,17 @@ def parse_args():
     parser.add_argument('input', help='Input .star file')
     parser.add_argument('--ind', required=True, help='Selected indices array (.pkl)')
     parser.add_argument('-o', help='Output .star file')
+    parser.add_argument('--relion31', action='store_true', help='Flag if relion3.1 star format')
     return parser
 
 def main(args):
-    s = starfile.Starfile.load(args.input, relion31=False)
+    s = starfile.Starfile.load(args.input, relion31=args.relion31)
     ind = utils.load_pkl(args.ind)
     log('{} particles'.format(len(s.df)))
     s.df = s.df.loc[ind]
     log(len(s.df))
     log(len(s.df.index))
-    s.write(args.o)
+    s.write(args.o, relion31=args.relion31)
 
 if __name__ == '__main__':
     main(parse_args().parse_args())

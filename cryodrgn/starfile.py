5ea8be81c5c23525d201226f0db4519f032cd6ba
Barrett Powell <bmp@mit.edu>
2021-07-16 13:08:16 -0400

    Added df_optics, header_optics attributes for RELION 3.1 .star files

--- cryodrgn/starfile.py
+++ cryodrgn/starfile.py
@@ -12,57 +12,87 @@ from .mrc import LazyImage
 
 class Starfile():
     
-    def __init__(self, headers, df):
+    def __init__(self, headers, df, headers_optics=None, df_optics=None):
         assert headers == list(df.columns), f'{headers} != {df.columns}'
         self.headers = headers
         self.df = df
+        if headers_optics is not None: # file is recognized as a RELION 3.1 star file
+            self.headers_optics = headers_optics
+            self.df_optics = df_optics
 
     @classmethod
     def load(self, starfile, relion31=False):
-        f = open(starfile,'r')
-        # get to data block
-        BLOCK = 'data_particles' if relion31 else 'data_'
-        while 1:
-            for line in f:
-                if line.startswith(BLOCK):
-                    break
-            break
-        # get to header loop
-        while 1:
-            for line in f:
-                if line.startswith('loop_'):
-                    break
-            break
-        # get list of column headers
-        while 1:
-            headers = []
-            for line in f:
-                if line.startswith('_'):
-                    headers.append(line)
-                else:
-                    break
-            break 
-        # assume all subsequent lines until empty line is the body
-        headers = [h.strip().split()[0] for h in headers]
-        body = [line]
-        for line in f:
-            if line.strip() == '':
+        def parse_starblock_to_df(filehandle, BLOCK):
+            # get to block
+            while 1:
+                for line in f:
+                    if line.startswith(BLOCK):
+                        assert line.strip().split()[0] == BLOCK, f"Expected block header: {BLOCK} but found: {line.strip().split()[0]}. Is this a RELION 3.1 starfile?"
+                        break
                 break
-            body.append(line)
-        # put data into an array and instantiate as dataframe
-        words = [l.strip().split() for l in body]
-        words = np.array(words)
-        assert words.ndim == 2, f"Uneven # columns detected in parsing {set([len(x) for x in words])}. Is this a RELION 3.1 starfile?" 
-        assert words.shape[1] == len(headers), f"Error in parsing. Number of columns {words.shape[1]} != number of headers {len(headers)}" 
-        data = {h:words[:,i] for i,h in enumerate(headers)}
-        df = pd.DataFrame(data=data)
-        return self(headers, df)
+            # get to header loop
+            while 1:
+                for line in f:
+                    if line.startswith('loop_'):
+                        break
+                break
+            # get list of column headers
+            while 1:
+                headers = []
+                for line in f:
+                    if line.startswith('_'):
+                        headers.append(line)
+                    else:
+                        break
+                break
+            # assume all subsequent lines until empty line is the body
+            headers = [h.strip().split()[0] for h in headers]
+            assert headers != [], "Could not find headers in file. Please double check if this is a RELION 3.1 starfile."
+            if line.strip() != '':
+                body = [line]
+            else:
+                body = []
+            for line in f:
+                if line.strip() == '':
+                    break
+                body.append(line)
+            # put data into an array and instantiate as dataframe
+            words = [l.strip().split() for l in body]
+            words = np.array(words)
+            assert words.shape[1] == len(headers), f"Error in parsing. Number of columns {words.shape[1]} != number of headers {len(headers)}"
+            data = {h: words[:, i] for i, h in enumerate(headers)}
+            df = pd.DataFrame(data=data)
+            return headers, df
 
-    def write(self, outstar):
+        f = open(starfile,'r')
+
+        # parse data_optics block if relion3.1 starfile
+        if relion31:
+            BLOCK = 'data_optics'
+            headers_optics, df_optics = parse_starblock_to_df(f, BLOCK)
+
+        # parse data block
+        BLOCK = 'data_particles' if relion31 else 'data_'
+        headers, df = parse_starblock_to_df(f, BLOCK)
+
+        if relion31:
+            return self(headers, df, headers_optics, df_optics)
+        else:
+            return self(headers, df)
+
+    def write(self, outstar, relion31=False):
         f = open(outstar,'w')
         f.write('# Created {}\n'.format(dt.now()))
         f.write('\n')
-        f.write('data_\n\n')
+        if relion31:
+            f.write('data_optics\n\n')
+            f.write('loop_\n')
+            f.write('\n'.join(self.headers_optics))
+            f.write('\n')
+            for i in self.df_optics.index:
+                f.write(' '.join([str(v) for v in self.df_optics.loc[i]]))
+                f.write('\n\n')
+        f.write('data_particles\n\n') if relion31 else f.write('data_\n\n')
         f.write('loop_\n')
         f.write('\n'.join(self.headers))
         f.write('\n')

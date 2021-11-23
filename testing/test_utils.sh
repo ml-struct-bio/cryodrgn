eeab1446eaa8972b5a089a9428bdaa7e2b259fe6
Ellen Zhong <ellenzhong@Ellens-MacBook-Pro.local>
2021-11-23 00:54:41 +0000

    Indexing bug in write_starfile.py when filtering a chunked dataset
    https://github.com/zhonge/cryodrgn/issues/88

--- testing/test_utils.sh
+++ testing/test_utils.sh
@@ -8,3 +8,8 @@ python ../utils/write_starfile.py data/toy_projections.mrcs test_ctf.pkl -o outp
 
 cryodrgn parse_pose_star output/test.star -D 30 -o output/test_pose.pkl
 #diff output/test_pose.pkl data/toy_rot_trans.pkl
+
+# .txt 
+cryodrgn downsample data/toy_projections.mrcs -D 28 --chunk 80 -o data/toy_projections.mrcs
+python ../utils/write_starfile.py data/toy_projections.txt test_ctf.pkl -o output/test2.star
+python ../utils/write_starfile.py data/toy_projections.txt test_ctf.pkl --ind data/ind100.pkl -o output/test2_100.star

12efcd52fcd7aca90791a2ddde86806b47304802
Ellen Zhong <zhonge@mit.edu>
2022-03-23 10:11:19 -0400

    Update tests

--- testing/test_utils.sh
+++ testing/test_utils.sh
@@ -4,10 +4,10 @@ set -x
 cryodrgn parse_ctf_star data/toy_projections.star -D 30 --Apix 1 -o test_ctf.pkl
 cryodrgn write_starfile data/toy_projections.mrcs test_ctf.pkl -o output/test.star
 cryodrgn write_starfile data/toy_projections.mrcs test_ctf.pkl -o output/test100.star --ind data/ind100.pkl 
-#cryodrgn write_starfile data/toy_projections.mrcs test_ctf.pkl -o output/test.star --poses data/toy_rot_trans.pkl
+cryodrgn write_starfile data/toy_projections.mrcs test_ctf.pkl -o output/test.star --poses data/toy_rot_trans.pkl
 
-#cryodrgn parse_pose_star output/test.star -D 30 -o output/test_pose.pkl
-#diff output/test_pose.pkl data/toy_rot_trans.pkl
+cryodrgn parse_pose_star output/test.star -D 30 -o output/test_pose.pkl
+python diff_cryodrgn_pkl.py output/test_pose.pkl data/toy_rot_trans.pkl
 
 # Test parsing from a .txt file 
 cryodrgn downsample data/toy_projections.mrcs -D 28 --chunk 80 -o data/toy_projections.mrcs

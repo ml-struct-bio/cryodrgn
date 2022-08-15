a74732c991a2446afa0dca725042bef3f6aee0f2
Vineet Bansal <vineetbansal@protonmail.com>
2022-08-15 15:04:15 -0400

    minimal changes for packaging/CI

--- cryodrgn/__init__.py
+++ cryodrgn/__init__.py
@@ -1,4 +1,7 @@
 import os
 
-__version__ = '1.1.0'
+# The _version.py file is managed by setuptools-scm
+#   and is not in version control.
+from ._version import version as __version__
+
 _ROOT = os.path.abspath(os.path.dirname(__file__))
---
+++ tests/test_mrc.py
@@ -0,0 +1,6 @@
+from cryodrgn import mrc, dataset
+import numpy as np
+
+
+def test_trivial():
+    assert 1+1 == 2

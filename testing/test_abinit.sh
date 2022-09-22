81fd1402e8187f26283310d8c60032b6de255f97
Adam Lerer <alerer@fb.com>
2022-09-22 11:59:47 -0700

    fix test_bnb

--- testing/test_abinit.sh
+++ testing/test_abinit.sh
@@ -3,5 +3,5 @@ set -e
 
 B=${BASH_SOURCE[0]}
 
-python $B/../cryodrgn/commands/abinit_het.py $B/data/hand.mrcs --zdim 8 -o $B/output/test
+python $B/../cryodrgn/commands/abinit_het.py $B/data/hand.mrcs --zdim 8 -o $B/output/test --multigpu
 python $B/../cryodrgn/commands/abinit_homo.py $B/data/hand.mrcs -o $B/output/test

be69f9979319fe1a00003362829c29d803e2b9aa
Ellen Zhong <zhonge@princeton.edu>
2023-02-12 13:41:11 -0500

    Update abinit tests to include analyze.py #223

--- testing/test_abinit.sh
+++ testing/test_abinit.sh
@@ -4,6 +4,8 @@ set -e
 # https://stackoverflow.com/questions/59895
 B=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python $B/../cryodrgn/commands/abinit_het.py $B/data/hand.mrcs --zdim 8 -o $B/output/test --multigpu --domain hartley
-python $B/../cryodrgn/commands/train_nn.py $B/data/hand.mrcs -o $B/output/test --domain hartley --uninvert-data --poses data/hand_rot.pkl --dim 256
-python $B/../cryodrgn/commands/abinit_homo.py $B/data/hand.mrcs -o $B/output/test --load $B/output/test/weights.pkl --domain hartley -n 40 --uninvert-data
+python $B/../cryodrgn/commands/abinit_het.py $B/data/hand.mrcs --zdim 8 -o $B/output/test_abinit_het --multigpu --domain hartley
+python $B/../cryodrgn/commands/analyze.py $B/output/test_abinit_het 0
+
+python $B/../cryodrgn/commands/train_nn.py $B/data/hand.mrcs -o $B/output/test_abinit_homo --domain hartley --uninvert-data --poses data/hand_rot.pkl --dim 256
+python $B/../cryodrgn/commands/abinit_homo.py $B/data/hand.mrcs -o $B/output/test_abinit_homo --load $B/output/test/weights.pkl --domain hartley -n 40 --uninvert-data

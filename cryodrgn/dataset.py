9141bf9876c0ba50a4812d4bdb79cc1ad7354387
Vineet Bansal <vineetbansal@protonmail.com>
2023-05-22 16:08:15 -0400

    Using a single thread for all loading (Upcoming DataShuffler obviates its use)

--- cryodrgn/dataset.py
+++ cryodrgn/dataset.py
@@ -30,7 +30,7 @@ class ImageDataset(data.Dataset):
         assert not keepreal, "Not implemented yet"
         datadir = datadir or ""
         self.src = ImageSource.from_file(
-            mrcfile, lazy=lazy, datadir=datadir, indices=ind, n_workers=max_threads
+            mrcfile, lazy=lazy, datadir=datadir, indices=ind, n_workers=1
         )
         if tilt_mrcfile is None:
             self.tilt_src = None

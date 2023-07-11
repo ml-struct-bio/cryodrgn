1b38f16c1597c6e5d013163455a75d2fdc7f2fdf
Ramya Rangan <rr1992@della-gpu.princeton.edu>
2023-07-11 13:04:55 -0400

    fix fsc and mse based map classification

--- analysis_scripts/classify_maps.py
+++ analysis_scripts/classify_maps.py
@@ -9,31 +9,39 @@ import torch
 
 logger = logging.getLogger(__name__)
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("vols1", help="Input")
     parser.add_argument("vols2", help="Input")
     parser.add_argument("--mask")
     parser.add_argument("--do-mse", action="store_true")
-    # parser.add_argument("-o", help="Output")
+    parser.add_argument("-o", help="Output")
     return parser
 
+def get_quantiles(vol):
+    vol_nonzero = vol[vol > 0]
+    quantiles = torch.quantile(vol_nonzero, torch.tensor([0.1, 0.9]))
+    return quantiles[0], quantiles[1]
 
 # TODO: implement diffmap normalization method here
 # Currently just rescales values to between 10th and 90th percentiles
-def normalize_vols(vols, vol_ref):
-    quartiles = torch.quartile(vol_ref, torch.tensor([0.1, 0.9]))
-    new_range = quartiles[1] - quartiles[0]
-    new_min = quartiles[0]
-    for ii, vol in vols:
-        min_val = torch.min(vol)
-        max_val = torch.max(vol)
+def normalize_vols(vols, vol_ref, mask_cutoff=0.001):
+    vol_ref[vol_ref < mask_cutoff] = 0
+    quantiles = get_quantiles(vol_ref)
+    new_range = quantiles[1] - quantiles[0]
+    new_min = quantiles[0]
+    for ii, vol in enumerate(vols):
+        vol[vol < mask_cutoff] = 0
+        quantiles_cur = get_quantiles(vol)
+        min_val = quantiles_cur[0]
+        max_val = quantiles_cur[1]
         vols[ii,:] = new_range * (vol - min_val)/(max_val - min_val) + new_min
 
     return vols
 
 
-def get_fsc(vol1, vol2, r, cutoff=cutoff):
+def get_fsc(vol1, vol2, r, D, cutoff=0.5, min_fsc_x=10):
     vol1 = fft.fftn_center(vol1)
     vol2 = fft.fftn_center(vol2)
 
@@ -49,22 +57,32 @@ def get_fsc(vol1, vol2, r, cutoff=cutoff):
         prev_mask = mask
     fsc = np.asarray(fsc)
     x = np.arange(D // 2) / D
-
-    res = 1.0/x[np.where(fsc < cutoff)[0][0]]
+    #print(fsc)
+    x_idx = -1
+    if fsc[-1] < cutoff:
+        fsc_above_cutoff = np.where(fsc < cutoff)[0]
+        idx = np.where(fsc_above_cutoff > min_fsc_x)[0][0]
+        x_idx = fsc_above_cutoff[idx]
+    res = 1.0/x[x_idx]
+    #print(np.where(fsc < cutoff))
+    #print(res)
     return res
 
-def get_pairwise_fsc(vols1, vols2, cutoff=0.5):
+
+def get_pairwise_fsc(vols1, vols2, D, cutoff=0.5):
     x = np.arange(-D // 2, D // 2)
     x2, x1, x0 = np.meshgrid(x, x, x, indexing="ij")
     coords = np.stack((x0, x1, x2), -1)
     r = (coords**2).sum(-1) ** 0.5
 
-    fsc = np.array(vols1.shape[0], vols2.shape[0])
+    fsc = np.zeros((vols1.shape[0], vols2.shape[0]))
     for ii, vol1 in enumerate(vols1):
+        print("Processing volume: %d of %d" % (ii + 1, vols1.shape[0]))
         for jj, vol2 in enumerate(vols2):
-            fsc[ii, jj] = get_fsc(vol1, vol2, r, cutoff=cutoff)
+            fsc[ii, jj] = get_fsc(vol1, vol2, r, D, cutoff=cutoff)
 
     return torch.from_numpy(fsc) 
+
 
 def main(args):
     vols1 = ImageSource.from_file(args.vols1)
@@ -72,10 +90,9 @@ def main(args):
     
     D = vols1.D
     assert D == vols2.D
-
-    vols1 = vols1.images().view(-1, D*D*D)
-    vols2 = vols2.images().view(-1, D*D*D)
-
+    
+    vols1 = vols1.images().view(-1, D, D, D)
+    vols2 = vols2.images().view(-1, D, D, D)
 
     if args.mask:
         mask = ImageSource.from_file(args.mask)
@@ -85,18 +102,29 @@ def main(args):
         mask = mask.to(torch.bool)
 
         vols1 *= mask.repeat(vols1.shape[0], 1, 1, 1)
-        vols2 *= mask.repeat(vols2.shape[0], 1, 1, 1).view(-1, D*D*D)
+        vols2 *= mask.repeat(vols2.shape[0], 1, 1, 1)
 
-    vol_ref = vols2[0:]
-    vols1_norm = normalize_vols(vols1, vol_ref).view(-1, D*D*D)
-    vols2_norm = normalize_vols(vols2, vol_ref).view(-1, D*D*D)
-    mse = torch.cdist(vols1_norm, vols2_norm, p=2)**2/(D*D*D)
-    
-    fsc = get_pairwise_fsc(vols1, vols2, D)
-
-    classes = torch.argmax(fsc, dim=1)
     if args.do_mse:
-        classes = torch.argmax(mse, dim=1)
+        vols1 = vols1.view(-1, D*D*D)
+        vols2 = vols2.view(-1, D*D*D)
+        
+        vol_ref = vols2[0,:]
+        print(vols1.shape)
+        print(vols2.shape)
+        print(vol_ref.shape)
+        
+        vols1_norm = normalize_vols(vols1, vol_ref).view(-1, D*D*D)
+        vols2_norm = normalize_vols(vols2, vol_ref).view(-1, D*D*D)
+        mse = torch.cdist(vols1_norm, vols2_norm, p=2)**2/(D*D*D)
+        print(mse)
+        np.savetxt(args.o, mse, fmt='%.4f')
+        classes = torch.argmin(mse, dim=1)
+    else:
+        fsc = get_pairwise_fsc(vols1, vols2, D)
+        # print(fsc)
+
+        np.savetxt(args.o, fsc, fmt='%.4f')
+        classes = torch.argmin(fsc, dim=1)
 
     num_classes = []
     for ii in range(vols2.shape[0]):

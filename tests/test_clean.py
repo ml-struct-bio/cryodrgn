5a73d9e7a5af5f44a9ec0472e733e552db82a950
Michal Grzadkowski <mgrzad@gmail.com>
2024-02-08 14:07:12 -0500

    more tests for cleaning and more sophisticated fixture teardowns

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -19,10 +19,12 @@ def run_command(cmd: str) -> tuple[str, str]:
 
 
 class TrainDir:
-    def __init__(self, data_lbl: str, train_cmd: str) -> None:
-        self.data_lbl = data_lbl
-        self.outdir = os.path.abspath(data_lbl)
+    def __init__(self, out_lbl: str, train_cmd: str, data_lbl: str = "toy") -> None:
+        self.out_lbl = out_lbl
+        self.outdir = os.path.abspath(out_lbl)
         self.train_cmd = train_cmd
+        self.data_lbl = data_lbl
+        self.orig_cache = os.path.join(self.outdir, "orig_cache")
 
         if self.data_lbl == "toy":
             particles = os.path.join(DATA_DIR, "toy_projections.mrcs")
@@ -36,13 +38,23 @@ class TrainDir:
         )
 
         if train_cmd == "train_vae":
-            cmd += "--zdim=4 --tdim=32 --tlayers=2 "
+            cmd += "--zdim=2 --tdim=16 --tlayers=1 "
         elif train_cmd == "train_nn":
-            cmd += "--dim=32 --layers=2 "
+            cmd += "--dim=16 --layers=2 "
 
         out, err = run_command(cmd)
+        if ") Finished in 0" not in out:
+            print(err)
         assert ") Finished in 0" in out
         assert self.all_files_present
+
+        orig_files = self.out_files
+        os.makedirs(self.orig_cache)
+        for orig_file in orig_files:
+            shutil.copy(
+                os.path.join(self.outdir, orig_file),
+                os.path.join(self.orig_cache, orig_file),
+            )
 
     @property
     def out_files(self) -> list[str]:
@@ -60,10 +72,10 @@ class TrainDir:
         if f"weights{epoch_lbl}.pkl" in out_files:
             cleaned = False
 
-        if self.data_lbl == "train_nn":
+        if self.train_cmd == "train_nn":
             if f"reconstruct{epoch_lbl}.mrc" in out_files:
                 cleaned = False
-        elif self.data_lbl == "train_vae":
+        elif self.train_cmd == "train_vae":
             if f"z{epoch_lbl}.pkl" in out_files:
                 cleaned = False
 
@@ -72,6 +84,13 @@ class TrainDir:
     @property
     def all_files_present(self) -> bool:
         return not any(self.epoch_cleaned(epoch) for epoch in list(range(10)) + [None])
+
+    def replace_files(self) -> None:
+        for orig_file in os.listdir(self.orig_cache):
+            shutil.copy(
+                os.path.join(self.orig_cache, orig_file),
+                os.path.join(self.outdir, orig_file),
+            )
 
 
 @pytest.fixture
@@ -83,10 +102,18 @@ def train_dir(request) -> TrainDir:
     shutil.rmtree(data_lbl)
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def toy_dir(request) -> TrainDir:
-    yield TrainDir("toy", request.param)
-    shutil.rmtree("toy")
+    toy_lbl = f"toy_{request.param}"
+    yield TrainDir(toy_lbl, request.param)
+    shutil.rmtree(toy_lbl)
+
+
+@pytest.fixture(scope="module")
+def toy_dirs(request) -> list[TrainDir]:
+    yield [TrainDir(f"toys_{train_cmd}", train_cmd) for train_cmd in request.param]
+    for train_cmd in request.param:
+        shutil.rmtree(f"toys_{train_cmd}")
 
 
 @pytest.mark.parametrize("toy_dir", ["train_nn", "train_vae"], indirect=True)
@@ -94,14 +121,14 @@ def toy_dir(request) -> TrainDir:
 def test_clean_here(toy_dir, every_n: int) -> None:
     """Test that we can clean the output of the current directory."""
 
-    os.chdir("toy")
+    os.chdir(toy_dir.out_lbl)
     out, err = run_command(f"cryodrgn_utils clean -n {every_n} -d")
     os.chdir("..")
     assert out == f"\tWould remove {2 * (9 - 9 // every_n)} files!\n"
     assert err == ""
     assert toy_dir.all_files_present
 
-    os.chdir("toy")
+    os.chdir(toy_dir.out_lbl)
     out, err = run_command(f"cryodrgn_utils clean -n {every_n}")
     os.chdir("..")
 
@@ -112,21 +139,56 @@ def test_clean_here(toy_dir, every_n: int) -> None:
     for epoch in range(10):
         assert toy_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
 
+    toy_dir.replace_files()
+
 
 @pytest.mark.parametrize("toy_dir", ["train_nn", "train_vae"], indirect=True)
 @pytest.mark.parametrize("every_n", [1, 2, 5])
 def test_clean_one(toy_dir, every_n: int) -> None:
     """Test that we can clean the output of one directory."""
 
-    out, err = run_command(f"cryodrgn_utils clean toy -n {every_n} -d")
+    out, err = run_command(f"cryodrgn_utils clean {toy_dir.out_lbl} -n {every_n} -d")
     assert out == f"\tWould remove {2 * (9 - (9 // every_n))} files!\n"
     assert err == ""
     assert toy_dir.all_files_present
 
-    out, err = run_command(f"cryodrgn_utils clean toy -n {every_n}")
+    out, err = run_command(f"cryodrgn_utils clean {toy_dir.out_lbl} -n {every_n}")
     assert out == f"\tRemoved {2 * (9 - (9 // every_n))} files!\n"
     assert err == ""
 
     assert not toy_dir.epoch_cleaned(None)
     for epoch in range(10):
         assert toy_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
+
+    toy_dir.replace_files()
+
+
+@pytest.mark.parametrize("toy_dirs", [("train_nn", "train_vae")], indirect=True)
+@pytest.mark.parametrize("every_n", [2, 4])
+def test_clean_two(toy_dirs, every_n: int) -> None:
+    """Test that we can clean the output of two directories."""
+
+    dir_str = " ".join([toy_dir.out_lbl for toy_dir in toy_dirs])
+    out, err = run_command(f"cryodrgn_utils clean {dir_str} -n {every_n} -d")
+
+    assert out == (
+        f"\tWould remove {2 * (9 - (9 // every_n))} files!\n"
+        f"\tWould remove {2 * (9 - (9 // every_n))} files!\n"
+    )
+    assert err == ""
+    for toy_dir in toy_dirs:
+        assert toy_dir.all_files_present
+
+    out, err = run_command(f"cryodrgn_utils clean {dir_str} -n {every_n}")
+    assert out == (
+        f"\tRemoved {2 * (9 - (9 // every_n))} files!\n"
+        f"\tRemoved {2 * (9 - (9 // every_n))} files!\n"
+    )
+    assert err == ""
+
+    for toy_dir in toy_dirs:
+        assert not toy_dir.epoch_cleaned(None)
+        for epoch in range(10):
+            assert toy_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
+
+        toy_dir.replace_files()

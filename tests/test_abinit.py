f5c77d91095ad97504d0389f2e37ba48df8b6769
Michal Grzadkowski <mgrzad@gmail.com>
2024-02-09 20:29:47 -0500

    refactoring ab-initio integration tests around teardown fixtures

--- tests/test_abinit.py
+++ tests/test_abinit.py
@@ -1,144 +1,117 @@
-import argparse
+"""Integration tests of ab initio reconstruction and downstream analyses.
+
+The training done here has unrealistically low parameter values to allow the tests to
+run in any environment in a reasonable amount of time with or without GPUs.
+
+"""
+import pytest
 import os
-import os.path
-from cryodrgn.commands import (
-    abinit_het,
-    abinit_homo,
-    analyze,
-    backproject_voxel,
-    view_config,
+import shutil
+from typing import Optional, Any
+from cryodrgn.utils import run_command
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "testing", "data")
+
+
+class AbInitioDir:
+    def __init__(
+        self,
+        zdim: int,
+        dataset: str = "hand",
+        epochs: int = 2,
+        seed: Optional[int] = None,
+    ) -> None:
+        self.zdim = zdim
+        self.dataset = dataset
+        self.epochs = epochs
+        self.seed = seed
+
+        self.outdir = os.path.abspath(f"test-output_{dataset}")
+        os.makedirs(self.outdir)
+
+        if self.dataset == "toy":
+            self.particles = os.path.join(DATA_DIR, "toy_projections.mrcs")
+        elif self.dataset == "hand":
+            self.particles = os.path.join(DATA_DIR, "hand.mrcs")
+        else:
+            raise ValueError(f"Unrecognized dataset label `{self.dataset}`!")
+
+    @classmethod
+    def parse_request(cls, req: dict[str, Any]) -> dict[str, Any]:
+        train_args = dict()
+
+        if "zdim" not in req:
+            raise ValueError("AbinitioDir fixture request must specify a zdim!")
+        train_args["zdim"] = req["zdim"]
+
+        if "dataset" in req:
+            train_args["dataset"] = req["dataset"]
+        else:
+            train_args["dataset"] = "hand"
+
+        if "epochs" in req:
+            train_args["epochs"] = req["epochs"]
+        else:
+            train_args["epochs"] = 2
+
+        if "seed" in req:
+            train_args["seed"] = req["seed"]
+        else:
+            train_args["seed"] = None
+
+        return train_args
+
+    def train(self, load_epoch: Optional[int] = None) -> None:
+        train_cmd = "abinit_het" if self.zdim > 0 else "abinit_homo"
+
+        cmd = (
+            f"cryodrgn {train_cmd} {self.particles} -o {self.outdir} "
+            f"--num-epochs {self.epochs} --no-window --pretrain 100 "
+        )
+        if self.zdim > 0:
+            cmd += f"--zdim {self.zdim} "
+            cmd += "--enc-dim 8 --enc-layers 2 --dec-dim 8 --pe-dim 8 "
+        else:
+            cmd += "--dim 16 --pe-dim 8 "
+
+        if self.seed is not None:
+            cmd += f"--seed={self.seed} "
+
+        if load_epoch is not None:
+            cmd += f"--load {os.path.join(self.outdir, f'weights.{load_epoch}.pkl')} "
+            cmd += (
+                f"--load-poses {os.path.join(self.outdir, f'pose.{load_epoch}.pkl')} "
+            )
+
+        out, err = run_command(cmd)
+        assert ") Finished in " in out, err
+
+    def analyze(self, analysis_epoch: int) -> None:
+        run_command(f"cryodrgn analyze {self.outdir} {analysis_epoch}")
+
+    def backproject(self) -> None:
+        run_command(
+            f"cryodrgn backproject_voxel {self.particles} "
+            f"-o {os.path.join(self.outdir, 'backproject', 'vol.mrc')} "
+            f"--poses {os.path.join(self.outdir, 'pose.pkl')} "
+        )
+
+    def view_config(self) -> None:
+        run_command(f"cryodrgn view_config {self.outdir}")
+
+
+@pytest.fixture(scope="session")
+def abinit_dir(request) -> AbInitioDir:
+    adir = AbInitioDir(**AbInitioDir.parse_request(request.param))
+    yield adir
+    shutil.rmtree(adir.outdir)
+
+
+@pytest.mark.parametrize(
+    "abinit_dir", [{"zdim": zdim} for zdim in [0, 4, 8]], indirect=True
 )
-
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "..", "testing", "data")
-
-"""
-These tests have unrealistically low parameter values to allow the tests to run on consumer-grade machines quickly,
-with or without GPUs.
-"""
-
-
-def test_abinit_het_and_backproject():
-    os.makedirs("output", exist_ok=True)
-
-    abinit_args = [
-        "--zdim",
-        "8",
-        "-o",
-        "output/abinit_het",
-        "--multigpu",  # ok to specify, will be ignored if no GPU(s).
-        "--enc-dim",
-        "8",
-        "--enc-layers",
-        "2",
-        "--dec-dim",
-        "8",
-        "--pe-dim",
-        "8",
-        "--num-epochs",
-        "1",
-        "--no-window",
-        "--pretrain",
-        "1000",
-    ]
-
-    args = abinit_het.add_args(argparse.ArgumentParser()).parse_args(
-        [
-            f"{DATA_FOLDER}/hand.mrcs",
-        ]
-        + abinit_args
-    )
-    abinit_het.main(args)
-
-    # Load a check-pointed model
-    abinit_het.main(
-        abinit_het.add_args(argparse.ArgumentParser()).parse_args(
-            [
-                f"{DATA_FOLDER}/hand.mrcs",
-                "--load",
-                "output/abinit_het/weights.0.pkl",
-                "--load-poses",
-                "output/abinit_het/pose.0.pkl",
-            ]
-            + abinit_args
-        )
-    )
-
-    args = analyze.add_args(argparse.ArgumentParser()).parse_args(
-        [
-            "output/abinit_het",
-            "0",  # Epoch number to analyze - 0-indexed
-        ]
-    )
-    analyze.main(args)
-
-    parser = argparse.ArgumentParser()
-    backproject_voxel.add_args(parser)
-    args = parser.parse_args(
-        [
-            f"{DATA_FOLDER}/hand.mrcs",
-            "--poses",
-            "output/abinit_het/pose.pkl",
-            "-o",
-            "output/abinit_het/backproject/vol.mrc",
-        ]
-    )
-    backproject_voxel.main(args)
-
-    args = view_config.add_args(argparse.ArgumentParser()).parse_args(
-        [
-            "output/abinit_het",
-        ]
-    )
-    view_config.main(args)
-
-
-def test_abinit_homo_and_backproject():
-    os.makedirs("output", exist_ok=True)
-
-    abinit_args = [
-        "-o",
-        "output/abinit_homo",
-        "--dim",
-        "16",
-        "--pe-dim",
-        "8",
-        "--num-epochs",
-        "1",
-        "--pretrain",
-        "1000",
-    ]
-
-    args = abinit_homo.add_args(argparse.ArgumentParser()).parse_args(
-        [
-            f"{DATA_FOLDER}/hand.mrcs",
-        ]
-        + abinit_args
-    )
-    abinit_homo.main(args)
-
-    # Load a check-pointed model
-    abinit_homo.main(
-        abinit_homo.add_args(argparse.ArgumentParser()).parse_args(
-            [
-                f"{DATA_FOLDER}/hand.mrcs",
-                "--load",
-                "output/abinit_homo/weights.0.pkl",
-                "--load-poses",
-                "output/abinit_homo/pose.0.pkl",
-            ]
-            + abinit_args
-        )
-    )
-
-    parser = argparse.ArgumentParser()
-    backproject_voxel.add_args(parser)
-    args = parser.parse_args(
-        [
-            f"{DATA_FOLDER}/hand.mrcs",
-            "--poses",
-            "output/abinit_homo/pose.pkl",
-            "-o",
-            "output/abinit_homo/backproject/vol.mrc",
-        ]
-    )
-    backproject_voxel.main(args)
+def test_analysis_and_backproject(abinit_dir):
+    abinit_dir.train()
+    abinit_dir.train(load_epoch=0)
+    abinit_dir.backproject()
+    abinit_dir.view_config()

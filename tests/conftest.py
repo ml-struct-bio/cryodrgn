f2c79fddbca8880a71f7ba500b535fab3c779bfa
Michal Grzadkowski <mgrzad@gmail.com>
2024-02-09 22:23:13 -0500

    consolidating reconstruction tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -185,3 +185,97 @@ def trained_dirs(train_dirs) -> list[TrainDir]:
     yield train_dirs
     for train_dir in train_dirs:
         train_dir.replace_files()
+
+
+class AbInitioDir:
+    def __init__(
+        self,
+        zdim: int,
+        dataset: str = "hand",
+        epochs: int = 2,
+        seed: Optional[int] = None,
+    ) -> None:
+        self.zdim = zdim
+        self.dataset = dataset
+        self.epochs = epochs
+        self.seed = seed
+
+        self.outdir = os.path.abspath(f"test-output_{dataset}")
+        os.makedirs(self.outdir)
+
+        if self.dataset == "toy":
+            self.particles = os.path.join(DATA_DIR, "toy_projections.mrcs")
+        elif self.dataset == "hand":
+            self.particles = os.path.join(DATA_DIR, "hand.mrcs")
+        else:
+            raise ValueError(f"Unrecognized dataset label `{self.dataset}`!")
+
+    @classmethod
+    def parse_request(cls, req: dict[str, Any]) -> dict[str, Any]:
+        train_args = dict()
+
+        if "zdim" not in req:
+            raise ValueError("AbinitioDir fixture request must specify a zdim!")
+        train_args["zdim"] = req["zdim"]
+
+        if "dataset" in req:
+            train_args["dataset"] = req["dataset"]
+        else:
+            train_args["dataset"] = "hand"
+
+        if "epochs" in req:
+            train_args["epochs"] = req["epochs"]
+        else:
+            train_args["epochs"] = 2
+
+        if "seed" in req:
+            train_args["seed"] = req["seed"]
+        else:
+            train_args["seed"] = None
+
+        return train_args
+
+    def train(self, load_epoch: Optional[int] = None) -> None:
+        train_cmd = "abinit_het" if self.zdim > 0 else "abinit_homo"
+
+        cmd = (
+            f"cryodrgn {train_cmd} {self.particles} -o {self.outdir} "
+            f"--num-epochs {self.epochs} --no-window --pretrain 100 "
+        )
+        if self.zdim > 0:
+            cmd += f"--zdim {self.zdim} "
+            cmd += "--enc-dim 8 --enc-layers 2 --dec-dim 8 --pe-dim 8 "
+        else:
+            cmd += "--dim 16 --pe-dim 8 "
+
+        if self.seed is not None:
+            cmd += f"--seed={self.seed} "
+
+        if load_epoch is not None:
+            cmd += f"--load {os.path.join(self.outdir, f'weights.{load_epoch}.pkl')} "
+            cmd += (
+                f"--load-poses {os.path.join(self.outdir, f'pose.{load_epoch}.pkl')} "
+            )
+
+        out, err = run_command(cmd)
+        assert ") Finished in " in out, err
+
+    def analyze(self, analysis_epoch: int) -> None:
+        run_command(f"cryodrgn analyze {self.outdir} {analysis_epoch}")
+
+    def backproject(self) -> None:
+        run_command(
+            f"cryodrgn backproject_voxel {self.particles} "
+            f"-o {os.path.join(self.outdir, 'backproject', 'vol.mrc')} "
+            f"--poses {os.path.join(self.outdir, 'pose.pkl')} "
+        )
+
+    def view_config(self) -> None:
+        run_command(f"cryodrgn view_config {self.outdir}")
+
+
+@pytest.fixture(scope="session")
+def abinit_dir(request) -> AbInitioDir:
+    adir = AbInitioDir(**AbInitioDir.parse_request(request.param))
+    yield adir
+    shutil.rmtree(adir.outdir)

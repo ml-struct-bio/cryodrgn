f9928f5f4f07cbc2f59179ba50cb30fdb441705f
Michal Grzadkowski <mgrzad@gmail.com>
2024-02-11 15:04:17 -0500

    cleaning up cleaning tests and adding tests of FSC output file consistency

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -7,90 +7,90 @@ from cryodrgn.utils import run_command
 
 @pytest.mark.parametrize("train_dir", [{"train_cmd": "train_nn"}], indirect=True)
 @pytest.mark.parametrize("every_n", [1, 2, 3, 5])
-def test_clean_here(train_dir, every_n: int) -> None:
+def test_clean_here(trained_dir, every_n: int) -> None:
     """Test that we can clean the output of the current directory."""
 
-    os.chdir(train_dir.out_lbl)
+    os.chdir(trained_dir.out_lbl)
     out, err = run_command(f"cryodrgn_utils clean -n {every_n} -d")
     os.chdir("..")
     assert out == f"\tWould remove {2 * (9 - 9 // every_n)} files!\n"
     assert err == ""
-    assert train_dir.all_files_present
+    assert trained_dir.all_files_present
 
-    os.chdir(train_dir.out_lbl)
+    os.chdir(trained_dir.out_lbl)
     out, err = run_command(f"cryodrgn_utils clean -n {every_n}")
     os.chdir("..")
 
     assert out == f"\tRemoved {2 * (9 - 9 // every_n)} files!\n"
     assert err == ""
 
-    assert not train_dir.epoch_cleaned(None)
+    assert not trained_dir.epoch_cleaned(None)
     for epoch in range(10):
-        assert train_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
-
-    train_dir.replace_files()
+        assert trained_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
 
 
 @pytest.mark.parametrize(
-    "train_dir",
-    [
-        {"train_cmd": "train_nn"},
-        {"train_cmd": "train_vae"},
-    ],
-    indirect=True
+    "train_dir", [{"train_cmd": "train_nn"}, {"train_cmd": "train_vae"}], indirect=True
 )
 @pytest.mark.parametrize("every_n", [1, 2, 5])
-def test_clean_one(train_dir, every_n: int) -> None:
+def test_clean_one(trained_dir, every_n: int) -> None:
     """Test that we can clean the output of one directory."""
 
-    out, err = run_command(f"cryodrgn_utils clean {train_dir.out_lbl} -n {every_n} -d")
-    assert out == f"\tWould remove {2 * (9 - (9 // every_n))} files!\n"
+    out, err = run_command(
+        f"cryodrgn_utils clean {trained_dir.out_lbl} -n {every_n} -d"
+    )
+
+    rmv_count = 2 * (trained_dir.epochs - 1 - ((trained_dir.epochs - 1) // every_n))
+    assert out == f"\tWould remove {rmv_count} files!\n"
     assert err == ""
-    assert train_dir.all_files_present
+    assert trained_dir.all_files_present
 
-    out, err = run_command(f"cryodrgn_utils clean {train_dir.out_lbl} -n {every_n}")
-    assert out == f"\tRemoved {2 * (9 - (9 // every_n))} files!\n"
+    out, err = run_command(f"cryodrgn_utils clean {trained_dir.out_lbl} -n {every_n}")
+    assert out == f"\tRemoved {rmv_count} files!\n"
     assert err == ""
 
-    assert not train_dir.epoch_cleaned(None)
-    for epoch in range(10):
-        assert train_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
-
-    train_dir.replace_files()
+    assert not trained_dir.epoch_cleaned(None)
+    for epoch in range(trained_dir.epochs):
+        assert trained_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
 
 
 @pytest.mark.parametrize(
     "train_dirs",
     [
-        (("hand", "train_nn"), ("toy", "train_vae")),
+        (
+            {"dataset": "hand", "train_cmd": "train_vae", "epochs": 4},
+            {"dataset": "toy", "train_cmd": "train_nn", "epochs": 11},
+        )
     ],
     indirect=True,
 )
-@pytest.mark.parametrize("every_n", [2, 4])
-def test_clean_two(train_dirs, every_n: int) -> None:
+@pytest.mark.parametrize("every_n", [2, 3])
+def test_clean_two(trained_dirs, every_n: int) -> None:
     """Test that we can clean the output of two directories."""
 
-    dir_str = " ".join([train_dir.out_lbl for train_dir in train_dirs])
+    dir_str = " ".join([trained_dir.out_lbl for trained_dir in trained_dirs])
     out, err = run_command(f"cryodrgn_utils clean {dir_str} -n {every_n} -d")
 
+    rmv_counts = [
+        2 * (trained_dir.epochs - 1 - ((trained_dir.epochs - 1) // every_n))
+        for trained_dir in trained_dirs
+    ]
+
     assert out == (
-        f"\tWould remove {2 * (9 - (9 // every_n))} files!\n"
-        f"\tWould remove {2 * (9 - (9 // every_n))} files!\n"
+        f"\tWould remove {rmv_counts[0]} files!\n"
+        f"\tWould remove {rmv_counts[1]} files!\n"
     )
     assert err == ""
-    for train_dir in train_dirs:
-        assert train_dir.all_files_present
+    for trained_dir in trained_dirs:
+        assert trained_dir.all_files_present
 
     out, err = run_command(f"cryodrgn_utils clean {dir_str} -n {every_n}")
     assert out == (
-        f"\tRemoved {2 * (9 - (9 // every_n))} files!\n"
-        f"\tRemoved {2 * (9 - (9 // every_n))} files!\n"
+        f"\tRemoved {rmv_counts[0]} files!\n" f"\tRemoved {rmv_counts[1]} files!\n"
     )
     assert err == ""
 
-    for train_dir in train_dirs:
-        assert not train_dir.epoch_cleaned(None)
-        for epoch in range(10):
-            assert train_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
-
-        train_dir.replace_files()
+    for trained_dir in trained_dirs:
+        assert not trained_dir.epoch_cleaned(None)
+        for epoch in range(trained_dir.epochs):
+            assert trained_dir.epoch_cleaned(epoch) == (epoch % every_n != 0)
--- tests/test_fsc.py
+++ tests/test_fsc.py
@@ -8,21 +8,19 @@ from cryodrgn.utils import run_command
 
 @pytest.mark.parametrize(
     "train_dir",
-    [
-        {"train_cmd": "train_nn", "epochs": 5, "seed": 1030},
-    ],
+    [{"train_cmd": "train_nn", "epochs": 5, "seed": 1030}],
     indirect=True,
 )
-def test_within_train(train_dir) -> None:
+def test_fidelity(trained_dir) -> None:
     """Test that we can compare two volumes produced during reconstruction training."""
 
-    vol_file1 = os.path.join(train_dir.outdir, "reconstruct.0.mrc")
-    vol_file2 = os.path.join(train_dir.outdir, "reconstruct.4.mrc")
-    fsc_file = os.path.join(train_dir.outdir, "fsc.txt")
+    vol_file1 = os.path.join(trained_dir.outdir, "reconstruct.0.mrc")
+    vol_file2 = os.path.join(trained_dir.outdir, "reconstruct.4.mrc")
+    fsc_file = os.path.join(trained_dir.outdir, "fsc.txt")
 
     out0, err = run_command(f"cryodrgn_utils fsc {vol_file1} {vol_file2}")
 
-    assert err == ""
+    assert err == "", err
     assert float(out0.split()[4]) == 0.0
     assert float(out0.split()[5]) == 1.0
     assert float(out0.split()[8]) == 0.031250
@@ -45,7 +43,7 @@ def test_within_train(train_dir) -> None:
     out, err = run_command(f"cryodrgn_utils fsc {vol_file1} {vol_file2} -o {fsc_file}")
     fsc_vals = np.fromfile(fsc_file, dtype=float, sep=" ")
 
-    assert err == ""
+    assert err == "", err
     assert out.split()[4] == "0.5:"
     assert round(float(out.split()[5]), 6) == 2.064516
     assert out.split()[10] == "0.143:"
@@ -53,5 +51,28 @@ def test_within_train(train_dir) -> None:
     assert len(out.split("\n")) == 3
     assert round(10 ** (fsc_vals @ np.tile([1, -1], len(fsc_vals) // 2)), 5) == 0.99311
 
+
+@pytest.mark.parametrize(
+    "train_dir",
+    [
+        {"dataset": "hand", "train_cmd": "train_nn", "epochs": 3, "seed": 2456},
+        {"dataset": "toy", "train_cmd": "train_nn", "epochs": 3, "seed": 2456},
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize("epochs", [(0, 1), (0, 2), (1, 2)])
+def test_output_file(trained_dir, epochs: tuple[int, int]) -> None:
+    vol_file1 = os.path.join(trained_dir.outdir, f"reconstruct.{epochs[0]}.mrc")
+    vol_file2 = os.path.join(trained_dir.outdir, f"reconstruct.{epochs[1]}.mrc")
+    fsc_file = os.path.join(trained_dir.outdir, "fsc.txt")
+
+    out0, err = run_command(f"cryodrgn_utils fsc {vol_file1} {vol_file2}")
+    assert err == "", err
+    assert out0.split()[-8] == "0.5:"
+    assert out0.split()[-2] == "0.143:"
+
+    out, err = run_command(f"cryodrgn_utils fsc {vol_file1} {vol_file2} -o {fsc_file}")
+    assert err == "", err
+    fsc_vals = np.fromfile(fsc_file, dtype=float, sep=" ")
     for i, fsc_val in enumerate(fsc_vals):
         assert round(fsc_val, 6) == round(float(out0.split()[4 + 2 * i + 1]), 6)

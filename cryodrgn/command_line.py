59ee154ae91170c37ed774ede61aff43651af187
Michal Grzadkowski <mgrzad@gmail.com>
2024-03-26 12:03:38 -0400

    better presentation of help strings for commands

--- cryodrgn/command_line.py
+++ cryodrgn/command_line.py
@@ -16,8 +16,15 @@ import cryodrgn
 
 
 def _get_commands(cmd_dir: str, doc_str: str = "") -> None:
-    parser = argparse.ArgumentParser(description=doc_str)
+    """Start up a parser using the modules in a cryoDRGN directory as subparsers.
 
+    Arguments
+    ---------
+        cmd_dir: path to folder containing cryoDRGN command modules
+        doc_str: documentation for this list of commands as a whole
+
+    """
+    parser = argparse.ArgumentParser(description=doc_str)
     parser.add_argument(
         "--version", action="version", version="cryoDRGN " + cryodrgn.__version__
     )
@@ -27,16 +34,24 @@ def _get_commands(cmd_dir: str, doc_str: str = "") -> None:
     module_files = os.listdir(cmd_dir)
     dir_lbl = os.path.basename(cmd_dir)
 
+    # look for Python modules that have the "add_args" method defined, which is what we
+    # use to mark a module in these directories as added to the command namespace
     for module_file in module_files:
         if module_file != "__init__.py" and module_file[-3:] == ".py":
             module_name = ".".join(["cryodrgn", dir_lbl, module_file[:-3]])
             module = import_module(module_name)
 
             if hasattr(module, "add_args"):
+                parsed_doc = module.__doc__.split("\n") if module.__doc__ else list()
+                descr_txt = parsed_doc[0] if parsed_doc else ""
+                epilog_txt = "" if len(parsed_doc) <= 1 else "\n".join(parsed_doc[1:])
+
+                # we add documentation text parsed from the module's docstring
                 this_parser = subparsers.add_parser(
                     module_file[:-3],
-                    description=module.__doc__,
-                    formatter_class=argparse.RawDescriptionHelpFormatter,
+                    description=descr_txt,
+                    epilog=epilog_txt,
+                    formatter_class=argparse.RawTextHelpFormatter,
                 )
                 module.add_args(this_parser)
                 this_parser.set_defaults(func=module.main)

653e29ecd1f7f0dc6df6d67aaefb860d4992dde0
Michal Grzadkowski <mgrzad@gmail.com>
2024-04-29 20:55:52 -0400

    adding testing for numpy.int --ind case; only deploy release upon pushing tag to main

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -298,8 +298,9 @@ class TestAbinitHetero:
     [
         None,
         os.path.join(DATA_FOLDER, "ind3.pkl"),
+        os.path.join(DATA_FOLDER, "ind3-numpy.pkl"),
     ],
-    ids=("no-ind", "ind3"),
+    ids=("no-ind", "ind3", "ind3-numpy"),
 )
 class TestSta:
     """Run reconstructions using particles from a .star file as input."""

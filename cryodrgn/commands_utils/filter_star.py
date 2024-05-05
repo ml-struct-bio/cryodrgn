3eb805539d358b48b6c24c02dbde191b37e54d72
Michal Grzadkowski <mgrzad@gmail.com>
2024-05-05 19:20:20 -0400

    cleaning up filtering stars to separate micrograph files

--- cryodrgn/commands_utils/filter_star.py
+++ cryodrgn/commands_utils/filter_star.py
@@ -25,8 +25,17 @@ def add_args(parser):
     parser.add_argument(
         "--et", action="store_true", help="Set if .star file includes tilts"
     )
-    parser.add_argument("--tomo", action="store_true", help="Split output by micrograph name into separate .star files")
-    parser.add_argument("-o", type=os.path.abspath, help="Output .star file")
+    parser.add_argument(
+        "--micrograph-files",
+        "-m",
+        action="store_true",
+        help="Split output by micrograph name into separate .star files",
+    )
+    parser.add_argument(
+        "-o",
+        type=os.path.abspath,
+        help="Output .star file (or directory if --micrograph-files)",
+    )
 
 
 def main(args):
@@ -34,33 +43,29 @@ def main(args):
     ind = utils.load_pkl(args.ind)
 
     if args.et:
-        particles_to_tilts, tilts_to_particles = TiltSeriesData.parse_particle_tilt(
-            args.input
-        )
-
-        ind = utils.load_pkl(args.ind)
+        particles_to_tilts, _ = TiltSeriesData.parse_particle_tilt(args.input)
         tilt_indices = TiltSeriesData.particles_to_tilts(particles_to_tilts, ind)
         df = s.df.loc[tilt_indices]
-
-    if args.tomo:
-        df = s.df.loc[ind]
-        os.makedirs(args.o, exist_ok=True)  # Ensure the output directory exists
-        for micrograph_name, group_df in df.groupby('_rlnMicrographName'):
-            filename_without_extension = os.path.splitext(micrograph_name)[0]  # Remove extension
-            output_path = os.path.join(args.o, f"{filename_without_extension}.star")
-            new_star = starfile.Starfile(headers=None, df=group_df)
-            new_star.write(output_path)
-            logger.info(f"Written .star file for {filename_without_extension} to {output_path}")
-        exit()
     else:
         df = s.df.loc[ind]
 
-    s = starfile.Starfile(headers=None, df=df)
-    s.write(args.o)
+    if args.micrograph_files:
+        os.makedirs(args.o, exist_ok=True)
+
+        for micrograph_name, group_df in df.groupby("_rlnMicrographName"):
+            filename_without_extension = os.path.splitext(micrograph_name)[0]
+            output_path = os.path.join(args.o, f"{filename_without_extension}.star")
+            new_star = starfile.Starfile(headers=None, df=group_df)
+            new_star.write(output_path)
+            logger.info(
+                f"Wrote .star file for {filename_without_extension} to {output_path}"
+            )
+    else:
+        s = starfile.Starfile(headers=None, df=df)
+        s.write(args.o)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     add_args(parser)
     main(parser.parse_args())
-

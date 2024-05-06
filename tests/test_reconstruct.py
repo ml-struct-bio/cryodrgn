81240b82ada2fd8877f54c21debdf8ef30727b4a
Michal Grzadkowski <mgrzad@gmail.com>
2024-05-05 22:02:16 -0400

    adding stand-alone tests of backprojection and homogeneous reconstruction

--- tests/test_reconstruct.py
+++ tests/test_reconstruct.py
@@ -1,10 +1,40 @@
 """Integration tests of ab initio reconstruction and downstream analyses.
 
-The training done here has unrealistically low parameter values to allow the tests to
-run in any environment in a reasonable amount of time with or without GPUs.
+Note that the training done here has unrealistically low parameter values to allow the
+tests to run in any environment in a reasonable amount of time with or without GPUs.
 
 """
 import pytest
+import os
+import argparse
+from cryodrgn.commands import backproject_voxel, train_nn
+
+
+@pytest.mark.parametrize(
+    "particles", ["toy.mrcs", "toy.star", "toy.txt"], indirect=True
+)
+@pytest.mark.parametrize("poses", ["toy-poses", "toy-angles"], indirect=True)
+@pytest.mark.parametrize("batch_size", ["24", "36"], ids=("batch24", "batch36"))
+@pytest.mark.parametrize("use_amp", [False, True], ids=("no-amp", "use-amp"))
+def test_homogeneous_with_poses(outdir, particles, poses, batch_size, use_amp):
+    args = [
+        particles,
+        "-o",
+        outdir,
+        "-n",
+        "10",
+        "--poses",
+        poses,
+        "--dim",
+        "12",
+        "-b",
+        batch_size,
+    ]
+    if not use_amp:
+        args += ["--no-amp"]
+
+    train_nn.main(train_nn.add_args(argparse.ArgumentParser()).parse_args(args))
+    assert "weights.9.pkl" in os.listdir(outdir)
 
 
 @pytest.mark.parametrize(
@@ -26,3 +56,26 @@ def test_abinit_checkpoint_analysis_and_backproject(abinit_dir):
 @pytest.mark.parametrize("train_epochs", [4, 5, 6])
 def test_frompose_train_and_from_checkpoint(trained_dir, load_epoch, train_epochs):
     trained_dir.train_load_epoch(load_epoch, train_epochs)
+
+
+@pytest.mark.parametrize(
+    "particles, poses, ctf",
+    [
+        ("toy.mrcs", "toy-poses", None),
+        ("hand", "hand-rot", None),
+        ("hand", "hand-poses", None),
+        ("tilts.star", "tilt-poses", "CTF-Tilt"),
+    ],
+    indirect=True,
+)
+def test_backprojection(outdir, particles, poses, ctf):
+    args = [particles, "--poses", poses, "-o", os.path.join(outdir, "vol.mrc")]
+
+    if ctf is not None:
+        args += ["--ctf", ctf]
+    if "tilt" in particles:
+        args += ["--tilt", "-d", "2.93"]
+
+    parser = argparse.ArgumentParser()
+    backproject_voxel.add_args(parser)
+    backproject_voxel.main(parser.parse_args(args))

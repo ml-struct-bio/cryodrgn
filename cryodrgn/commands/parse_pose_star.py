c0605e41d282a932cff3471dbcb2b720bbee4e38
Michal Grzadkowski <mgrzad@gmail.com>
2024-05-20 18:49:39 -0400

    adding support for RELION 3.1 optics groups in parse_pose_star

--- cryodrgn/commands/parse_pose_star.py
+++ cryodrgn/commands/parse_pose_star.py
@@ -5,6 +5,9 @@ This command is often used as a part of preparing inputs for training commands s
 
 Example usages
 --------------
+$ cryodrgn parse_pose_star particles_from_M.star -o pose.pkl
+
+# override image parameters even if given in file
 $ cryodrgn parse_pose_star particles_from_M.star -o pose.pkl -D 294 --Apix 1.7
 
 """
@@ -29,18 +32,14 @@ def add_args(parser):
         help="Output pose.pkl",
     )
 
-    parser.add_argument(
-        "--ignore-optics",
-        action="store_true",
-        help="ignore presence of optics tables from RELION 3.1 format",
-    )
-
     group = parser.add_argument_group("Optionally provide missing image parameters")
-    group.add_argument("-D", type=int, help="Box size of reconstruction (pixels)")
+    group.add_argument(
+        "-D", type=int, help="override box size of reconstruction (pixels)"
+    )
     group.add_argument(
         "--Apix",
         type=float,
-        help="Pixel size (A); Required if translations are specified in Angstroms",
+        help="Pixel size (A); override if translations are specified in Angstroms",
     )
     return parser
 
@@ -51,32 +50,45 @@ def main(args):
     if not args.outpkl.endswith(".pkl"):
         raise ValueError("Output file must be a .pkl file (pickled Python format)!")
 
-    s = starfile.Starfile.load(args.input)
-    if s.relion31:  # Get image stats from data_optics table
-        assert s.data_optics is not None
-
-        if len(s.data_optics.df) > 1:
-            if not args.ignore_optics:
-                raise ValueError(
-                    "Datasets with only one optics group are supported "
-                    "— use --ignore-optics flag if necessary!"
-                )
-
-        args.Apix = float(s.data_optics.df["_rlnImagePixelSize"][0])
-        args.D = int(float(s.data_optics.df["_rlnImageSize"][0]))
-
-    if args.D is None and "_rlnImageSize" in s.headers:
-        args.D = int(float(s.df["_rlnImageSize"][0]))
-    assert args.D is not None, "Must provide image size with -D"
-
-    N = len(s.df)
+    resolution = None
+    apix = None
+    stardata = starfile.Starfile.load(args.input)
+    N = len(stardata.df)
     logger.info(f"{N} particles")
+
+    if stardata.relion31:  # Get image stats from data_optics table
+        assert stardata.data_optics is not None
+        optics_df = stardata.data_optics.df.set_index("_rlnOpticsGroup")
+        apix = np.array(
+            [
+                float(optics_df.loc[g, "_rlnImagePixelSize"])
+                for g in stardata.df["_rlnOpticsGroup"].values
+            ]
+        )
+        resolution = np.array(
+            [
+                int(float(optics_df.loc[g, "_rlnImageSize"]))
+                for g in stardata.df["_rlnOpticsGroup"].values
+            ]
+        )
+
+    if resolution is None and "_rlnImageSize" in stardata.headers:
+        resolution = np.array(
+            [int(float(stardata.df["_rlnImageSize"][0])) for _ in range(N)]
+        )
+
+    if args.D is not None:
+        resolution = np.array([args.D for _ in range(N)])
+    if args.Apix is not None:
+        apix = np.array([args.Apix for _ in range(N)])
+
+    assert resolution is not None, "Must provide image size with -D"
 
     # parse rotations
     euler = np.zeros((N, 3))
-    euler[:, 0] = s.df["_rlnAngleRot"]
-    euler[:, 1] = s.df["_rlnAngleTilt"]
-    euler[:, 2] = s.df["_rlnAnglePsi"]
+    euler[:, 0] = stardata.df["_rlnAngleRot"]
+    euler[:, 1] = stardata.df["_rlnAngleTilt"]
+    euler[:, 2] = stardata.df["_rlnAnglePsi"]
     logger.info("Euler angles (Rot, Tilt, Psi):")
     logger.info(euler[0])
     logger.info("Converting to rotation matrix:")
@@ -86,30 +98,34 @@ def main(args):
 
     # parse translations
     trans = np.zeros((N, 2))
-    if "_rlnOriginX" in s.headers and "_rlnOriginY" in s.headers:
+    if "_rlnOriginX" in stardata.headers and "_rlnOriginY" in stardata.headers:
         # translations in pixels
-        trans[:, 0] = s.df["_rlnOriginX"]
-        trans[:, 1] = s.df["_rlnOriginY"]
+        trans[:, 0] = stardata.df["_rlnOriginX"]
+        trans[:, 1] = stardata.df["_rlnOriginY"]
 
-    elif "_rlnOriginXAngst" in s.headers and "_rlnOriginYAngst" in s.headers:
+    elif (
+        "_rlnOriginXAngst" in stardata.headers
+        and "_rlnOriginYAngst" in stardata.headers
+    ):
         # translation in Angstroms (Relion 3.1)
-        assert (
-            args.Apix is not None
-        ), "Must provide --Apix argument to convert _rlnOriginXAngst and _rlnOriginYAngst translation units"
-        trans[:, 0] = s.df["_rlnOriginXAngst"]
-        trans[:, 1] = s.df["_rlnOriginYAngst"]
-        trans /= args.Apix
-
+        assert apix is not None, (
+            "Must provide --Apix argument to convert _rlnOriginXAngst "
+            "and _rlnOriginYAngst translation units"
+        )
+        trans[:, 0] = stardata.df["_rlnOriginXAngst"]
+        trans[:, 1] = stardata.df["_rlnOriginYAngst"]
+        trans /= apix.reshape(-1, 1)
     else:
         logger.warning(
-            "Warning: Neither _rlnOriginX/Y nor _rlnOriginX/YAngst found. Defaulting to 0s."
+            "Warning: Neither _rlnOriginX/Y nor _rlnOriginX/YAngst found. "
+            "Defaulting to 0s."
         )
 
     logger.info("Translations (pixels):")
     logger.info(trans[0])
 
     # convert translations from pixels to fraction
-    trans /= args.D
+    trans /= resolution.reshape(-1, 1)
 
     # write output
     logger.info(f"Writing {args.outpkl}")

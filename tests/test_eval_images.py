ed37b100c09aee340c636f273fba909fda27ac28
Michal Grzadkowski <mgrzad@gmail.com>
2024-05-27 16:12:44 +1000

    fixtures for weights and configs

--- tests/test_eval_images.py
+++ tests/test_eval_images.py
@@ -1,30 +1,33 @@
-import os.path
-import argparse
 import pytest
+import os
+import argparse
 from cryodrgn.source import ImageSource
 from cryodrgn.commands import eval_images
-
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "..", "testing", "data")
 
 
 @pytest.fixture
 def mrcs_data():
-    return ImageSource.from_file(f"{DATA_FOLDER}/hand.mrcs").images()
+    return ImageSource.from_file(f"{pytest.data_dir}/hand.mrcs").images()
 
 
-def test_invert_contrast(mrcs_data):
+@pytest.mark.parametrize(
+    "particles, poses, weights, configs",
+    [("hand", "hand-rot", "het", "het")],
+    indirect=True,
+)
+def test_invert_contrast(tmpdir, particles, poses, weights, configs):
     args = eval_images.add_args(argparse.ArgumentParser()).parse_args(
         [
-            f"{DATA_FOLDER}/hand.mrcs",
-            f"{DATA_FOLDER}/het_weights.pkl",
+            particles.path,
+            weights.path,
             "--config",
-            f"{DATA_FOLDER}/het_config.pkl",
+            configs.path,
             "-o",
-            "output/out_eval_images_losses.pkl",
+            os.path.join(tmpdir, "out_eval_images_losses.pkl"),
             "--out-z",
-            "output/out_eval_images_z.pkl",
+            os.path.join(tmpdir, "out_eval_images_z.pkl"),
             "--poses",
-            f"{DATA_FOLDER}/hand_rot.pkl",
+            poses.path,
             "--log-interval",
             "1",
             "--verbose",

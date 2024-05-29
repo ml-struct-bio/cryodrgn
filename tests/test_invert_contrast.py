90c8877ebaf591d76e14a2b0205eec461b26ec15
Michal Grzadkowski <mgrzad@gmail.com>
2024-05-29 11:13:37 +1000

    removing deprecated preprocessing

--- tests/test_invert_contrast.py
+++ tests/test_invert_contrast.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 import shutil
 import numpy as np
@@ -6,18 +7,15 @@ from cryodrgn.source import ImageSource
 from cryodrgn.mrc import MRCFile
 from cryodrgn.utils import run_command
 
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "..", "testing", "data")
 
-
-def test_output():
+@pytest.mark.parametrize("volume", ["hand", "toy"], indirect=True)
+def test_output(tmpdir, volume):
     """Try different ways of specifying the output file."""
-    os.makedirs("output", exist_ok=True)
-
-    vol_file = os.path.join("output", "hand-vol.mrc")
-    shutil.copyfile(os.path.join(DATA_FOLDER, "hand-vol.mrc"), vol_file)
+    vol_file = os.path.join(tmpdir, "vol.mrc")
     inv_file = os.path.join(
-        "output", os.path.basename(vol_file).replace(".mrc", "_inverted.mrc")
+        tmpdir, os.path.basename(vol_file).replace(".mrc", "_inverted.mrc")
     )
+    shutil.copyfile(volume.path, vol_file)
 
     out, err = run_command(f"cryodrgn_utils invert_contrast {vol_file}")
     assert err == ""
@@ -28,40 +26,44 @@ def test_output():
     assert np.allclose(flipped_data, -mrcs_data)
 
     inv_file = os.path.join(
-        "output", "vols", os.path.basename(vol_file).replace(".mrc", "_inverted.mrc")
+        tmpdir, "vols", os.path.basename(vol_file).replace(".mrc", "_inverted.mrc")
     )
 
     out, err = run_command(f"cryodrgn_utils invert_contrast {vol_file} -o {inv_file}")
     assert err == ""
 
-    mrcs_data, _ = MRCFile.parse(vol_file)
+    mrcs_data, _ = MRCFile.parse(volume.path)
     flipped_data, _ = MRCFile.parse(inv_file)
     assert np.allclose(flipped_data, -mrcs_data)
 
 
-def test_mrc_file():
-    vol_file = os.path.join(DATA_FOLDER, "hand-vol.mrc")
+@pytest.mark.parametrize("volume", ["hand"], indirect=True)
+def test_mrc_file(tmpdir, volume):
     inv_file = os.path.join(
-        "output", os.path.basename(vol_file).replace(".mrc", "_inverted.mrc")
+        tmpdir, os.path.basename(volume.path).replace(".mrc", "_inverted.mrc")
     )
 
-    out, err = run_command(f"cryodrgn_utils invert_contrast {vol_file} -o {inv_file}")
+    out, err = run_command(
+        f"cryodrgn_utils invert_contrast {volume.path} -o {inv_file}"
+    )
     assert err == ""
 
-    mrcs_data, _ = MRCFile.parse(vol_file)
+    mrcs_data, _ = MRCFile.parse(volume.path)
     flipped_data, _ = MRCFile.parse(inv_file)
     assert np.allclose(flipped_data, -mrcs_data)
 
 
-def test_image_source():
-    vol_file = os.path.join(DATA_FOLDER, "toy_projections.mrc")
+@pytest.mark.parametrize("volume", ["hand"], indirect=True)
+def test_image_source(tmpdir, volume):
     inv_file = os.path.join(
-        "output", os.path.basename(vol_file).replace(".mrc", "_inverted.mrc")
+        tmpdir, os.path.basename(volume.path).replace(".mrc", "_inverted.mrc")
     )
 
-    out, err = run_command(f"cryodrgn_utils invert_contrast {vol_file} -o {inv_file}")
+    out, err = run_command(
+        f"cryodrgn_utils invert_contrast {volume.path} -o {inv_file}"
+    )
     assert err == ""
 
-    mrcs_data = ImageSource.from_file(vol_file).images()
+    mrcs_data = ImageSource.from_file(volume.path).images()
     flipped_data = ImageSource.from_file(inv_file).images()
     assert torch.allclose(flipped_data, -mrcs_data)

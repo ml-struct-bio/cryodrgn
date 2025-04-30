d30971bf73cc24a3f3b74db48d98a463dc96ec48
Ellen Zhong <zhonge@princeton.edu>
2025-04-30 17:33:23 +0000

    Fix hand dataset poses in tests

--- tests/quicktest.sh
+++ tests/quicktest.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 set -e
 set -x
-cryodrgn train_vae  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl --zdim 10 --pe-type gaussian
+
+# cryodrgn
+#cryodrgn train_vae  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot.pkl --zdim 10 --pe-type gaussian
+cryodrgn train_vae  data/hand.mrcs -o output/toy_recon_vae --lr .0001 --seed 0 --poses data/hand_rot_trans.pkl --ctf data/test_ctf.100.pkl --zdim 10
+cryodrgn analyze output/toy_recon_vae
+
+# drgnai-fixed
+cryodrgn setup output/toy_recon_vae_2 --particles data/hand.mrcs --poses data/hand_rot_trans.pkl --z-dim 10 --model cryodrgn-ai --pose-estimation fixed --cfg n_imgs_pose_search=50 --ctf data/test_ctf.100.pkl 
+cryodrgn train output/toy_recon_vae_2
